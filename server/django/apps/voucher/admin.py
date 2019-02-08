fa405482362b88c8a9192cb97f0e8678e61d9cf4
Dipesh Acharya <xtranophilist@gmail.com>
2019-02-08 18:45:58 +0545

    SalesVoucher migration and admin

--- server/django/apps/voucher/admin.py
+++ server/django/apps/voucher/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.voucher.models import SalesVoucher
+
+
+class SalesVoucherAdmin(admin.ModelAdmin):
+    search_fields = (
+        'voucher_no', 'party__name', 'party__tax_registration_number', 'customer_name', 'address', 'user__full_name',
+        'user__email', 'company__name', 'company__tax_registration_number', 'remarks', 'total_amount')
+    list_filter = ('company', 'status', 'mode')
+    list_display = ('company', 'voucher_no', 'party', 'customer_name', 'total_amount')
+
+
+admin.site.register(SalesVoucher, SalesVoucherAdmin)
--- server/django/apps/voucher/models.py
+++ server/django/apps/voucher/models.py
@@ -1,9 +1,8 @@
-from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
 from apps.ledger.models import Party
-from apps.users.models import Company
+from apps.users.models import Company, User
 
 STATUSES = (
     ('Draft', 'Draft'),
@@ -28,14 +27,18 @@ class SalesVoucher(models.Model):
     issue_datetime = models.DateTimeField(default=timezone.now)
     transaction_date = models.DateField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sales_vouchers')
-    status = models.CharField(choices=STATUSES, default=STATUSES[0][0])
-    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='sales_vouchers')
+    status = models.CharField(choices=STATUSES, default=STATUSES[0][0], max_length=15)
     remarks = models.TextField(blank=True, null=True)
     due_date = models.DateField(blank=True, null=True)
     discount = models.FloatField(default=0)
     total_amount = models.FloatField(null=True, blank=True)
-    mode = models.CharField(choices=STATUSES, default=MODES[0][0])
+    mode = models.CharField(choices=STATUSES, default=MODES[0][0], max_length=15)
+
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='sales_vouchers')
+
+    def __str__(self):
+        return str(self.voucher_no)
 
 
-class SalesVoucherRow(models.Model):
-    pass
+# class SalesVoucherRow(models.Model):
+#     pass
