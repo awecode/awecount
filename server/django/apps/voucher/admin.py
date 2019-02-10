720666cdd558a6e6dcaa05f8e17445733eb3617a
Roshan Shrestha <roshanshrestha01@gmail.com>
2019-02-10 16:23:07 +0545

    SaleVoucherRow register to admin, Added as inline to SaleVoucher

--- server/django/apps/voucher/admin.py
+++ server/django/apps/voucher/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from apps.voucher.models import SalesVoucher
+from apps.voucher.models import SalesVoucher, SalesVoucherRow
+
+
+class SaleVoucherRowTabular(admin.TabularInline):
+    model = SalesVoucherRow
 
 
 class SalesVoucherAdmin(admin.ModelAdmin):
@@ -9,6 +13,8 @@ class SalesVoucherAdmin(admin.ModelAdmin):
         'user__email', 'company__name', 'company__tax_registration_number', 'remarks', 'total_amount')
     list_filter = ('company', 'status', 'mode')
     list_display = ('company', 'voucher_no', 'party', 'customer_name', 'total_amount')
+    inlines = (SaleVoucherRowTabular,)
 
 
 admin.site.register(SalesVoucher, SalesVoucherAdmin)
+admin.site.register(SalesVoucherRow)
