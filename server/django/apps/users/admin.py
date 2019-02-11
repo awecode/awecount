a60ea999d27044135cc2e700cc39ae229f61e248
ankitch <ankitchhetri0@gmail.com>
2019-02-11 12:29:45 +0545

    added Password form to custom and user and fixed authentication

--- server/django/apps/users/admin.py
+++ server/django/apps/users/admin.py
@@ -1,20 +1,81 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin, UserChangeForm as DjangoUserChangeForm, \
+    UserCreationForm as DjangoUserCreationForm
 
 from .models import User, Company
+from django import forms
 
 admin.site.unregister(Group)
 
 
-class UserAdmin(admin.ModelAdmin):
-    search_fields = ('full_name', 'email', 'company__name')
-    list_filter = ('company', 'is_superuser')
-    list_display = ('full_name', 'email', 'company')
-    exclude = ('password',)
-    readonly_fields = ('last_login', 'date_joined', 'is_superuser')
+class UserCreationForm(DjangoUserCreationForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ("email",)
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
 
 
-admin.site.register(User, UserAdmin)
+class UserChangeForm(DjangoUserChangeForm):
+    class Meta(DjangoUserChangeForm.Meta):
+        model = User
+
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]
+
+
+class CustomUserAdmin(UserAdmin):
+    ordering = ('email',)
+    form = UserChangeForm
+    add_form = UserCreationForm
+    filter_horizontal = ()
+    list_display_links = ('id', 'full_name', 'email', 'date_joined',)
+    list_display = ('id', 'full_name', 'email', 'date_joined', 'company', 'is_superuser',)
+    list_filter = ('is_superuser',)
+    fieldsets = ((None,
+                  {'fields': ('full_name',
+                              'email',
+                              'password',
+                              'date_joined',
+                              'last_login',
+                              'company',
+                              'is_superuser',)}),
+                 )
+    add_fieldsets = ((None,
+                      {'fields': ('full_name',
+                                  'email',
+                                  'password1',
+                                  'password2',
+                                  'date_joined',
+                                  'is_superuser')}),
+                     )
+    search_fields = ('full_name', 'email', 'company', 'is_superuser',)
+    readonly_fields = ('is_superuser', 'date_joined', 'last_login',)
+
+
+admin.site.register(User, CustomUserAdmin)
 
 
 class CompanyAdmin(admin.ModelAdmin):
--- server/django/awecount/utils/JWTCustomAuthentication.py
+++ server/django/awecount/utils/JWTCustomAuthentication.py
@@ -1,16 +1,19 @@
+from datetime import datetime
 from django.contrib.auth import authenticate, login
 from rest_framework.exceptions import APIException
 from rest_framework.response import Response
 from rest_framework_jwt.serializers import jwt_payload_handler, jwt_encode_handler
-from rest_framework_jwt.views import ObtainJSONWebToken
+from rest_framework_jwt.settings import api_settings
+from rest_framework_jwt.views import ObtainJSONWebToken, jwt_response_payload_handler
+
+from apps.users.serializers import CompanySerializer, UserSerializer
 
 
 class JWTCustomAuthentication(ObtainJSONWebToken):
     def post(self, request, *args, **kwargs):
         data = request.data
-        user = authenticate(username=data.get('username'), password=data.get('password'))
+        user = authenticate(email=data.get('email'), password=data.get('password'))
         context = {}
-
         if user:
             payload = jwt_payload_handler(user)
             context = {
@@ -19,19 +22,13 @@ class JWTCustomAuthentication(ObtainJSONWebToken):
         else:
             raise APIException({'non_field_errors': 'You have enter wrong ID or password.'})
 
-        # Block user without role
-        if user and (not user.role or not user.role.modules):
-            raise APIException({'non_field_errors': ['User have no roles.']})
-        # Block web user to access mobile login
-        if user and 'HTTP_PLATFORM' in request.META and request.META.get(
-                'HTTP_PLATFORM') == 'Android' and not user.is_mobile_user:
-            raise APIException({'non_field_errors': ['You are not mobile user. Please check back office.']})
-        # Block mobile user to access web login
-        if user and 'HTTP_PLATFORM' not in request.META and user.is_mobile_user:
-            raise APIException({'non_field_errors': ['You are not web user. Please check back office.']})
-        # Update token json with user data
+        # Block user without company
+        if user and not user.company:
+            raise APIException({'non_field_errors': ['User not registered to any company    .']})
+
         if user and 'token' in context:
-            context.update(user.data)
+            context['company'] = CompanySerializer(user.company).data
+            context['user'] = UserSerializer(user).data
         if user is not None:
             login(request, user)
         return Response(context)
