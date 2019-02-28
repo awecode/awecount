f1d44f97f2ce4fb66b5fd9c7b18a4d2520ea0e86
Roshan Shrestha <roshanshrestha01@gmail.com>
2019-02-28 15:30:18 +0545

    Sale Voucher response options

--- server/django/apps/voucher/api.py
+++ server/django/apps/voucher/api.py
@@ -1,8 +1,9 @@
+import json
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from .models import SalesVoucher, SalesVoucherRow
+from .models import SalesVoucher, SalesVoucherRow, DISCOUNT_TYPES
 from .serializers import SalesVoucherCreateSerializer, SalesVoucherListSerializer
 from awecount.utils import get_next_voucher_no
 
@@ -22,5 +23,11 @@ class SalesVoucherViewSet(viewsets.ModelViewSet):
 
     @action(detail=False)
     def get_next_no(self, request):
-        order_no = get_next_voucher_no(SalesVoucher, 'voucher_no')
-        return Response({'order_no': order_no})
+        voucher_no = get_next_voucher_no(SalesVoucher, 'voucher_no')
+        return Response({'voucher_no': voucher_no})
+
+    @action(detail=False)
+    def options(self, request):
+        return Response({
+            'discount_types': [dict(value=type[0], text=type[1]) for type in DISCOUNT_TYPES]
+        })
