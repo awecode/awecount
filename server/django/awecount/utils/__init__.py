1891309295d3b4c98bf41918803265e2940f2452
Roshan Shrestha <roshanshrestha01@gmail.com>
2019-03-02 13:55:43 +0545

    borrowed account app models

--- server/django/awecount/utils/__init__.py
+++ server/django/awecount/utils/__init__.py
@@ -1,3 +1,6 @@
+from django.db import connection
+
+
 def get_next_voucher_no(cls, attr):
     from django.db.models import Max
 
@@ -6,3 +9,21 @@ def get_next_voucher_no(cls, attr):
         return max_voucher_no + 1
     else:
         return 1
+
+
+def zero_for_none(obj):
+    if obj is None or obj is '':
+        return 0
+    else:
+        return obj
+
+
+def none_for_zero(obj):
+    if not obj:
+        return None
+    else:
+        return obj
+
+
+def model_exists_in_db(model):
+    return model._meta.db_table in connection.introspection.table_names()
