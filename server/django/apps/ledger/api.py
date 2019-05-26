6387341b53b6d7cbed5acfbaff1c19c8f9338cb3
Roshan Shrestha <roshanshrestha01@gmail.com>
2019-05-26 14:31:20 +0545

    journal entries filter

--- server/django/apps/ledger/api.py
+++ server/django/apps/ledger/api.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django_filters import filters
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
@@ -7,8 +8,9 @@ from rest_framework.response import Response
 
 from apps.voucher.models import SalesVoucher
 from apps.voucher.serializers import SaleVoucherOptionsSerializer
-from .models import Account
-from .serializers import PartySerializer, AccountSerializer, AccountDetailSerializer, CategorySerializer
+from .models import Account, JournalEntry
+from .serializers import PartySerializer, AccountSerializer, AccountDetailSerializer, CategorySerializer, \
+    JournalEntrySerializer
 from awecount.utils.CustomViewSet import CreateListRetrieveUpdateViewSet
 from awecount.utils.mixins import InputChoiceMixin
 
@@ -42,4 +44,22 @@ class AccountViewSet(InputChoiceMixin, CreateListRetrieveUpdateViewSet):
     def project_fy_cr_ledger_choices(self, request, pk=None):
         instance = self.get_object()
         data = AccountSerializer(instance, many=True).data
+        return Response(data)
+
+    @action(detail=True, methods=['get'], url_path='journal-entries')
+    def journal_entries(self, request, pk=None):
+        param = request.GET
+        start_date = param.get('start_date')
+        start_date = datetime.strptime(start_date, '%Y-%m-%d')
+        end_date = param.get('end_date')
+        end_date = datetime.strptime(end_date, '%Y-%m-%d')
+        obj = self.get_object()
+        entries = JournalEntry.objects.filter(transactions__account_id=obj.pk).order_by('pk',
+                                                                                        'date') \
+            .prefetch_related('transactions', 'content_type', 'transactions__account').select_related()
+        if start_date == end_date:
+            entries = entries.filter(date=start_date)
+        else:
+            entries = entries.filter(date__range=[start_date, end_date])
+        data = JournalEntrySerializer(entries, context={'account': obj}, many=True).data
         return Response(data)
