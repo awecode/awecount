197d170fc092ffdbc7d1c35b1de5aeae1064fa49
Roshan Shrestha <roshanshrestha01@gmail.com>
2019-06-09 16:11:50 +0545

    Item serializer with unit_id

--- server/django/apps/product/serializers.py
+++ server/django/apps/product/serializers.py
@@ -5,11 +5,12 @@ from apps.product.models import Item, Unit
 
 class ItemSerializer(serializers.ModelSerializer):
     company_id = serializers.IntegerField()
-    tax_scheme_id = serializers.IntegerField(required=False)
+    tax_scheme_id = serializers.IntegerField(required=False, allow_null=True)
+    unit_id = serializers.IntegerField(required=False)
 
     class Meta:
         model = Item
-        exclude = ('company', 'tax_scheme',)
+        exclude = ('company', 'tax_scheme', 'unit',)
 
 
 class UnitSerializer(serializers.ModelSerializer):
