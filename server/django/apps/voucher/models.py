4cc1183c6d72082d2f6898a61e67fae8c6cbeec9
Roshan Shrestha <roshanshrestha01@gmail.com>
2019-07-25 16:37:45 +0545

    Voucher save with status in apply transaction

--- server/django/apps/voucher/models.py
+++ server/django/apps/voucher/models.py
@@ -2,7 +2,8 @@ from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils import timezone
 
-from apps.ledger.models import Party, Account, set_transactions as set_ledger_transactions, get_account, JournalEntry
+from apps.ledger.models import Party, Account, set_transactions as set_ledger_transactions, get_account, JournalEntry, \
+    Category
 from apps.product.models import Item, Unit
 from apps.tax.models import TaxScheme
 from apps.users.models import Company, User
@@ -38,6 +39,19 @@ class BankAccount(models.Model):
     branch_name = models.CharField(max_length=250, blank=True, null=True)
     company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='bank_accounts')
     ledger = models.ForeignKey(Account, null=True, on_delete=models.SET_NULL, related_name='bank_accounts')
+
+    def save(self, *args, **kwargs):
+        if not self.ledger:
+            ledger = Account(name=self.account_number, company=self.company)
+            try:
+                ledger.category = Category.objects.get(name='Bank Accounts', parent__name='Assets',
+                                                       company=self.company)
+            except Category.DoesNotExist:
+                pass
+            ledger.code = 'P-' + str(self.account_number)
+            ledger.save()
+            self.ledger = ledger
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return '{} : {}'.format(self.account_name, self.company.name)
@@ -115,7 +129,6 @@ class SalesVoucher(models.Model):
         row_ids = [row.id for row in self.rows.all()]
         JournalEntry.objects.filter(content_type=content_type, object_id__in=row_ids).delete()
 
-
     @staticmethod
     def apply_transactions(voucher):
         if voucher.status == 'Cancelled':
@@ -123,16 +136,23 @@ class SalesVoucher(models.Model):
         if not voucher.status == 'Issued':
             return
 
+        # TODO Also keep record of cash payment for party in party ledger [To show transactions for particular party]
         if voucher.mode == 'Credit':
             dr_acc = voucher.party.customer_account
         elif voucher.mode == 'Cash':
             dr_acc = get_account(voucher.company, 'Cash')
+            voucher.status = 'Paid'
+        # TODO Allow creating cheque deposit
         elif voucher.mode == 'Cheque':
-            pass
-        elif voucher.mode == 'ePayment':
-            pass
+            dr_acc = voucher.party.customer_account
+            voucher.status = 'Paid'
         elif voucher.mode == 'Bank Deposit':
-            pass
+            dr_acc = voucher.bank_account.account
+            voucher.status = 'Paid'
+        # elif voucher.mode == 'ePayment':
+        #     pass
+
+        voucher.save()
 
         dividend_discount = 0
 
