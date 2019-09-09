a7e71b1bb940455df4c4ff63fc21a95958243b4a
Dipesh Acharya <xtranophilist@gmail.com>
2019-09-09 15:49:22 +0545

    Update Cancel permission

--- server/django/apps/users/permissions.py
+++ server/django/apps/users/permissions.py
@@ -1,6 +1,8 @@
 from rest_framework import permissions
 from rest_framework.exceptions import PermissionDenied
 
+from .permission_modules import MODULES
+
 
 class ModuleAccessPermission(permissions.BasePermission):
     message = 'Permission not enough.'
@@ -16,14 +18,18 @@ class ModuleAccessPermission(permissions.BasePermission):
         list_permission = '{}View'.format(_model_name)
         create_permission = '{}Create'.format(_model_name)
         modify_permission = '{}Modify'.format(_model_name)
+        cancel_permission = '{}Cancel'.format(_model_name)
 
+        if request.method == 'POST' and cancel_permission in MODULES and cancel_permission not in modules:
+            raise PermissionDenied({'detail': 'User does not have permission to cancel %s.' % _model_name})
+        
         if request.method == 'GET' and list_permission not in modules:
             raise PermissionDenied({'detail': self.message})
 
         if request.method == 'POST' and create_permission not in modules:
-            raise PermissionDenied({'detail': 'User does not have permission to create %s' % _model_name})
+            raise PermissionDenied({'detail': 'User does not have permission to create %s.' % _model_name})
 
         if request.method == 'PUT' and modify_permission not in modules:
-            raise PermissionDenied({'detail': 'User does not have permission to update %s' % _model_name})
+            raise PermissionDenied({'detail': 'User does not have permission to update %s.' % _model_name})
 
         return True
