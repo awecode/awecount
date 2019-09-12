14a8f8ff25f7732117d6b67c2d6c219e563f66ab
Dipesh Acharya <xtranophilist@gmail.com>
2019-09-12 14:33:56 +0545

    Check Login for Export View

--- server/django/apps/aggregator/views.py
+++ server/django/apps/aggregator/views.py
@@ -1,8 +1,11 @@
+from django.core.exceptions import PermissionDenied
 from django.http import FileResponse
 from .export import get_zipped_csvs
 from datetime import datetime
 
 def export_data(request):
+    if not request.user.is_authenticated or not request.company_id:
+        raise PermissionDenied
     #TODO Verify password as well
     zipped_data = get_zipped_csvs(request.company_id)
     response = FileResponse(zipped_data)
