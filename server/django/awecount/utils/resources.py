8a2c27a364b5af5d69882d49248073ddae6bba49
Dipesh Acharya <xtranophilist@gmail.com>
2019-09-12 23:02:01 +0545

    PrettyNameModelResource

---
+++ server/django/awecount/utils/resources.py
@@ -0,0 +1,13 @@
+from import_export import resources
+
+
+class PrettyNameModelResource(resources.ModelResource):
+    @classmethod
+    def get_pretty_name(cls, name):
+        return name.replace('__', ' ').replace('_', ' ').title()
+
+    @classmethod
+    def field_from_django_field(cls, field_name, django_field, readonly):
+        field = super().field_from_django_field(field_name, django_field, readonly)
+        field.column_name = cls.get_pretty_name(field_name)
+        return field
