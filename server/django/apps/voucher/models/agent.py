9e74ea567134ae51bb097bc60da5bbef959237e1
Dipesh Acharya <xtranophilist@gmail.com>
2019-09-13 14:16:29 +0545

    Company for Sales Agent

--- server/django/apps/voucher/models/agent.py
+++ server/django/apps/voucher/models/agent.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+from apps.users.models import Company
+
 
 class SalesAgent(models.Model):
     name = models.CharField(max_length=255)
     compensation_multiplier = models.FloatField(default=1)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='sales_agents')
 
     def __str__(self):
         return self.name
