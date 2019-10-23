37c3ce96424a839c2223f7f1df475b427d71386b
ankitch <ankitchhetri0@gmail.com>
2019-10-23 13:39:56 +0545

    WIP json search

--- server/django/apps/product/filters.py
+++ server/django/apps/product/filters.py
@@ -1,9 +1,10 @@
 from django_filters import rest_framework as filters
 
-from .models import Item, InventoryAccount
+from .models import Item
 
 
 class ItemFilterSet(filters.FilterSet):
+
     class Meta:
         model = Item
-        fields = ('can_be_sold', 'can_be_purchased')
+        fields = ('can_be_sold', 'can_be_purchased', 'search_data',)
