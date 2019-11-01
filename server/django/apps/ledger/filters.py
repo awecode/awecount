58c0345c422d8cd1ba6ece852c4218eeb19ac76d
Dipesh Acharya <xtranophilist@gmail.com>
2019-11-01 15:23:20 +0545

    Default and has balance filters for account listing

--- server/django/apps/ledger/filters.py
+++ server/django/apps/ledger/filters.py
@@ -1,10 +1,22 @@
+from django.db.models import Q
 from django_filters import rest_framework as filters
 
-from apps.ledger.models import Category
+from apps.ledger.models import Category, Account
+
+
+class HasBalanceFilter(filters.BooleanFilter):
+    def filter(self, qs, value):
+        if value:
+            return qs.filter(Q(current_dr__isnull=False) | Q(current_cr__isnull=False))
+        return qs
 
 
 class AccountFilterSet(filters.FilterSet):
-    pass
+    hasbalance = HasBalanceFilter()
+
+    class Meta:
+        model = Account
+        fields = ('default',)
 
 
 class CategoryFilterSet(filters.FilterSet):
