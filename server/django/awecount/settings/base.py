c982e7074c29b772573de7724eba24a09aa604ab
Dipesh Acharya <xtranophilist@gmail.com>
2019-11-08 11:21:30 +0545

    mf

--- server/django/awecount/settings/base.py
+++ server/django/awecount/settings/base.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -34,7 +35,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'apps.users.middleware.CompanyMiddleware',
     'auditlog.middleware.AuditlogMiddleware',
 ]
