ca624b288cb1328595659802d001a7257f6fe7ef
Dipesh Acharya <xtranophilist@gmail.com>
2019-11-08 13:21:38 +0545

    Improve AccessKeyAuthentication

--- server/django/apps/users/authentication.py
+++ server/django/apps/users/authentication.py
@@ -7,7 +7,8 @@ from apps.users.models import AccessKey
 class AccessKeyAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
         secret = request.META.get('HTTP_SECRET')
-        user = AccessKey.get_user(secret)
-        if not user:
-            raise exceptions.AuthenticationFailed('Invalid secret key!')
-        return user, None
+        if secret:
+            user = AccessKey.get_user(secret)
+            if not user:
+                raise exceptions.AuthenticationFailed('Invalid secret key!')
+            return user, None
