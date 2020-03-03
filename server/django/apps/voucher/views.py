074e3344df50842b0f025bb47513a46c3f351f28
Dipesh Acharya <xtranophilist@gmail.com>
2020-03-03 19:52:23 +0545

    mc

--- server/django/apps/voucher/views.py
+++ server/django/apps/voucher/views.py
@@ -1,10 +1,6 @@
 from django.http import HttpResponse
-from django.template import Context
-from django.template.loader import get_template
-from io import BytesIO
-
 from django.views.generic.detail import DetailView
-from django_xhtml2pdf.utils import PdfResponse, fetch_resources
+from django_xhtml2pdf.utils import fetch_resources
 from django_xhtml2pdf.views import PdfMixin
 from xhtml2pdf import pisa
 
@@ -16,7 +12,7 @@ class SalesVoucherPdfView(PdfMixin, DetailView):
     template_name = "sale_voucher_pdf.html"
 
     def render(self):
-        retval = super(PdfResponse, self).render()
+        # retval = super(PdfResponse, self).render()
         response = HttpResponse(content_type='application/pdf')
         response['Content-Disposition'] = 'attachment; filename="report.pdf"'
         pisa.CreatePDF(
