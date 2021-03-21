d84f4fbf9dc0250a39669ce76179e28f360e7090
Dipesh Acharya <xtranophilist@gmail.com>
2021-03-22 00:15:44 +0545

    Count orphan journal entries

--- server/django/apps/ledger/management/commands/find_orphan_journal_entries.py
+++ server/django/apps/ledger/management/commands/find_orphan_journal_entries.py
@@ -7,6 +7,10 @@ class Command(BaseCommand):
     help = 'Find journal entries without source transaction'
 
     def handle(self, *args, **options):
+        cnt = 0
         for jv in JournalEntry.objects.all():
             if not jv.source:
+                cnt += 1
                 print(jv.id)
+
+        print('{} orphan journal entries!')
