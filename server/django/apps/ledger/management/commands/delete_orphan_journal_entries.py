3f31cc22dcff93491cd68c85fef2b2daa2dcbe11
Dipesh Acharya <xtranophilist@gmail.com>
2021-03-22 00:27:44 +0545

    mf

--- server/django/apps/ledger/management/commands/delete_orphan_journal_entries.py
+++ server/django/apps/ledger/management/commands/delete_orphan_journal_entries.py
@@ -4,14 +4,13 @@ from apps.ledger.models import JournalEntry
 
 
 class Command(BaseCommand):
-    help = 'Find journal entries without source transaction'
+    help = 'Delete journal entries without source transaction'
 
     def handle(self, *args, **options):
         cnt = 0
         for jv in JournalEntry.objects.all():
             if not jv.source:
                 cnt += 1
-                first = jv.transactions.first()
-                print(jv.id, jv.date, jv.content_type, (first.account, first.account.company) if first else '')
+                jv.delete()
 
-        print('{} orphan journal entries!'.format(cnt))
+        print('{} orphan journal entries deleted!'.format(cnt))
--- server/django/apps/ledger/management/commands/find_orphan_journal_entries.py
+++ server/django/apps/ledger/management/commands/find_orphan_journal_entries.py
@@ -4,13 +4,14 @@ from apps.ledger.models import JournalEntry
 
 
 class Command(BaseCommand):
-    help = 'Delete journal entries without source transaction'
+    help = 'Find journal entries without source transaction'
 
     def handle(self, *args, **options):
         cnt = 0
         for jv in JournalEntry.objects.all():
             if not jv.source:
                 cnt += 1
-                jv.delete()
+                first = jv.transactions.first()
+                print(jv.id, jv.date, jv.content_type, (first.account, first.account.company) if first else '')
 
-        print('{} orphan journal entries deleted!'.format(cnt))
+        print('{} orphan journal entries!'.format(cnt))
