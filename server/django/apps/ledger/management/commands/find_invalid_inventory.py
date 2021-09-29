b9ba9eaf04d9a594ef459579836cceffd612214d
Dipesh Acharya <xtranophilist@gmail.com>
2021-09-29 21:26:50 +0545

    Find obsolete inv transactions for sales

--- server/django/apps/ledger/management/commands/find_invalid_inventory.py
+++ server/django/apps/ledger/management/commands/find_invalid_inventory.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from django.db.models import Q
 
 from apps.product.models import find_obsolete_transactions
-from apps.voucher.models import PurchaseVoucher
+from apps.voucher.models import PurchaseVoucher, SalesVoucher
 
 
 class Command(BaseCommand):
@@ -11,6 +11,16 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         pvs = PurchaseVoucher.objects.all()
         for pv in pvs:
+            for row in pv.rows.filter(Q(item__track_inventory=True) | Q(item__fixed_asset=True)).select_related(
+                    'item__account'):
+                find_obsolete_transactions(
+                    row,
+                    pv.date,
+                    ['dr', row.item.account, int(row.quantity)],
+                )
+
+        svs = SalesVoucher.objects.all()
+        for pv in svs:
             for row in pv.rows.filter(Q(item__track_inventory=True) | Q(item__fixed_asset=True)).select_related(
                     'item__account'):
                 find_obsolete_transactions(
