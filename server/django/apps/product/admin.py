c77c1cdd19e2dbe4b7a309bf6cf4b4ff55badd62
Dipesh Acharya <xtranophilist@gmail.com>
2021-09-30 18:21:57 +0545

    Admin action to delete items full data

--- server/django/apps/product/admin.py
+++ server/django/apps/product/admin.py
@@ -18,11 +18,32 @@ def merge_brands(modeladmin, request, queryset):
 merge_brands.short_description = "Merge brands"
 
 
+def delete_item_data(modeladmin, request, queryset):
+    count = len(queryset)
+    if count > 1:
+        messages.warning(request, 'Please select only one item.')
+    else:
+        item = queryset[0]
+        item.account and item.account.delete()
+        item.sales_account and item.sales_account.delete()
+        item.purchase_account and item.purchase_account.delete()
+        item.discount_allowed_account and item.discount_allowed_account.delete()
+        item.discount_received_account and item.discount_received_account.delete()
+        item.expense_account and item.expense_account.delete()
+        item.fixed_asset_account and item.fixed_asset_account.delete()
+        item.delete()
+        messages.success(request, 'Deleted items!'.format(count))
+
+
+delete_item_data.short_description = "Delete Full Item Data"
+
+
 class ItemAdmin(admin.ModelAdmin):
     search_fields = ('name', 'code', 'description', 'selling_price', 'cost_price')
     list_filter = (
         'track_inventory', 'can_be_sold', 'can_be_purchased', 'fixed_asset', 'direct_expense', 'indirect_expense', 'brand')
     list_display = ('code', 'name', 'cost_price', 'selling_price', 'brand')
+    actions = (delete_item_data,)
 
 
 class UnitAdmin(admin.ModelAdmin):
