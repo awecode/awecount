be6549e583b93467b62fbceea6476f520e826e3e
Pramod-115 <pramodthapa115@gmail.com>
2023-02-17 00:02:22 +0545

    fix: minor fixes

--- web/app/src/components/voucher/InvoiceTable.vue
+++ web/app/src/components/voucher/InvoiceTable.vue
@@ -9,41 +9,39 @@
           <div class="col-2 text-center">Amnt</div>
           <div class="col-2 text-center"></div>
         </div>
-        <div
-          class="row q-col-gutter-md"
-          v-for="(row, index) in rows"
-          :key="index"
-        >
-          <div class="col-grow">
-            <n-auto-complete
-              v-model="fields"
-              :options="formDefaults"
-              label="Party"
-              :error="errors?.party"
-              :modal-component="PartyForm"
-            />
+        <div v-for="(row, index) in rows" :key="index">
+          <div class="row q-col-gutter-md">
+            <div class="col-grow">
+              <n-auto-complete v-model="fields" :options="props.itemOptions" label="Party" :error="errors?.party"
+                :modal-component="PartyForm" />
+            </div>
+            <div class="col-2">
+              <q-input v-model="fields" class="col-md-6 col-12" label="Address" :error-message="errors"
+                :error="!!errors"></q-input>
+            </div>
+            <div class="col-2">
+              <q-input v-model="fields" class="col-md-6 col-12" label="Address" :error-message="errors"
+                :error="!!errors"></q-input>
+            </div>
+            <div class="col-2">asas</div>
+            <div class="col-2">asas</div>
           </div>
-          <div class="col-2">
-            <q-input
-              v-model="fields"
-              class="col-md-6 col-12"
-              label="Address"
-              :error-message="errors"
-              :error="!!errors"
-            ></q-input>
+          <div class="row q-col-gutter-md q-md-px-xl">
+            <div class="col-grow">
+              <n-auto-complete v-model="fields" :options="props.itemOptions" label="Party" :error="errors?.party"
+                :modal-component="PartyForm" />
+            </div>
+            <div class="col-2">
+              <q-input v-model="fields" class="col-md-6 col-12" label="Address" :error-message="errors"
+                :error="!!errors"></q-input>
+            </div>
+            <div class="col-2">
+              <q-input v-model="fields" class="col-md-6 col-12" label="Address" :error-message="errors"
+                :error="!!errors"></q-input>
+            </div>
           </div>
-          <div class="col-2">
-            <q-input
-              v-model="fields"
-              class="col-md-6 col-12"
-              label="Address"
-              :error-message="errors"
-              :error="!!errors"
-            ></q-input>
-          </div>
-          <div class="col-2">asas</div>
-          <div class="col-2">asas</div>
         </div>
+        {{ props.itemOptions }} --opt
       </div>
     </q-card>
   </q-card-section>
@@ -52,10 +50,32 @@
 <script>
 export default {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  props: {
+    itemOptions: {
+      type: Object,
+      default: () => { return {} },
+    },
+    modelValue: {
+      type: Array,
+      default: () => [{
+        discount: '',
+        quantity: '',
+        rate: '',
+        item_id: '',
+        unit_id: '',
+        description: '',
+        discount: '',
+        discount_type: null,
+        tax_scheme_id: ''
+      }]
+    }
+  },
+
   setup(props, context) {
     const rows = ref(1)
     return {
       rows,
+      props
     }
   },
 }
