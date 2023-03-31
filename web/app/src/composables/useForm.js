7eae798e79b1e22004cb08dc12cf30df116c7db4
Pramod <pramodthapa115@gmail.com>
2023-03-31 14:01:08 +0545

    fix: fix composable to send create defaults in fields

--- web/app/src/composables/useForm.js
+++ web/app/src/composables/useForm.js
@@ -40,12 +40,11 @@ export default (endpoint, config) => {
         }
       )
     }
-
     if (config.getDefaults) {
       useApi(getDefaultsFetchUrl()).then((data) => {
         // TODO: Check with Dipesh sir
         if (data.fields) {
-          // fields.value = data.fields
+          fields.value = Object.assign(fields.value, data.fields)
         }
         formDefaults.value = data
         // TODO: Check with Dipesh sir
