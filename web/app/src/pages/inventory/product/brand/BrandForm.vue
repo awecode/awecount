6cfe4a714741c1fd2099810efc9ed4fd7393a1cf
Pramod <pramodthapa115@gmail.com>
2023-06-22 12:04:05 +0545

    feat: add permissions in brand

--- web/app/src/pages/inventory/product/brand/BrandForm.vue
+++ web/app/src/pages/inventory/product/brand/BrandForm.vue
@@ -11,32 +11,19 @@
       <q-card class="q-mx-lg q-pt-md">
         <q-card-section>
           <div class="row q-col-gutter-md">
-            <q-input
-              v-model="fields.name"
-              label="Name *"
-              class="col-6"
-              :error-message="errors.name"
-              :error="!!errors.name"
-            />
+            <q-input v-model="fields.name" label="Name *" class="col-6" :error-message="errors.name"
+              :error="!!errors.name" />
           </div>
           <div>
-            <q-input
-              v-model="fields.description"
-              label="Description"
-              class="col-6"
-              :error-message="errors.description"
-              :error="!!errors.description"
-              type="textarea"
-            />
+            <q-input v-model="fields.description" label="Description" class="col-6" :error-message="errors.description"
+              :error="!!errors.description" type="textarea" />
           </div>
         </q-card-section>
         <div class="text-right q-pr-md q-pb-lg">
-          <q-btn
-            @click.prevent="submitForm"
-            color="primary"
-            :label="isEdit ? 'Update' : 'Create'"
-            class="q-ml-auto"
-          />
+          <q-btn v-if="checkPermissions('BrandModify') && isEdit" @click.prevent="submitForm" color="primary"
+            label="Update" class="q-ml-auto" />
+          <q-btn v-if="!isEdit && checkPermissions('BrandCreate')" @click.prevent="submitForm" color="primary"
+            :label="isEdit ? 'Update' : 'Create'" class="q-ml-auto" />
         </div>
       </q-card>
     </q-card>
@@ -45,6 +32,7 @@
 
 <script>
 import useForm from '/src/composables/useForm'
+import checkPermissions from 'src/composables/checkPermissions'
 export default {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   setup(props, context) {
@@ -57,7 +45,7 @@ export default {
       ...useForm(endpoint, {
         getDefaults: true,
         successRoute: '/brand/',
-      }),
+      }), checkPermissions
     }
   },
 }
--- web/app/src/pages/inventory/product/brand/BrandList.vue
+++ web/app/src/pages/inventory/product/brand/BrandList.vue
@@ -2,33 +2,17 @@
   <div class="q-pa-md">
     <div class="row justify-between">
       <div></div>
-      <q-btn
-        color="green"
-        to="/brand/add/"
-        label="New brand"
-        class="q-ml-lg"
-        icon-right="add"
-      />
+      <q-btn v-if="checkPermissions('BrandCreate')" color="green" to="/brand/add/" label="New brand" class="q-ml-lg"
+        icon-right="add" />
     </div>
-    <q-table
-      :rows="rows"
-      :columns="columns"
-      :loading="loading"
-      :filter="searchQuery"
-      v-model:pagination="pagination"
-      row-key="id"
-      @request="onRequest"
-      class="q-mt-md"
-      :rows-per-page-options="[20]"
-    >
+    <q-table :rows="rows" :columns="columns" :loading="loading" :filter="searchQuery" v-model:pagination="pagination"
+      row-key="id" @request="onRequest" class="q-mt-md" :rows-per-page-options="[20]">
       <template v-slot:body-cell-name="props">
         <q-td :props="props">
-          <router-link
-            class="text-blue"
-            style="text-decoration: none"
-            :to="`/brand/${props.row.id}/`"
-            >{{ props.row.name }}</router-link
-          >
+          <router-link v-if="checkPermissions('BrandModify')" class="text-blue" style="text-decoration: none"
+            :to="`/brand/${props.row.id}/`">{{ props.row.name
+            }}</router-link>
+          <span v-else>{{ props.row.name }}</span>
         </q-td>
       </template>
     </q-table>
@@ -37,6 +21,7 @@
 
 <script>
 import useList from '/src/composables/useList'
+import checkPermissions from 'src/composables/checkPermissions'
 export default {
   setup() {
     const metaData = {
@@ -44,7 +29,7 @@ export default {
     }
     useMeta(metaData)
     const endpoint = '/v1/brands/'
-    return { ...useList(endpoint) }
+    return { ...useList(endpoint), checkPermissions }
   },
 }
 </script>
