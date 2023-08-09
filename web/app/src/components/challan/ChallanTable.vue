00329bfd4ab5d3963d80cd6c0b212741e25fde7f
Pramod <pramodthapa115@gmail.com>
2023-08-09 17:11:55 +0545

    fix: fix challan row unit id

--- web/app/src/components/challan/ChallanTable.vue
+++ web/app/src/components/challan/ChallanTable.vue
@@ -16,115 +16,56 @@
           <div class="col-1 text-center"></div>
         </div>
         <div v-for="(row, index) in modalValue" :key="index">
-          <!-- <InvoiceRow
-            v-if="modalValue[index]"
-            :usedIn="props.usedIn"
-            v-model="modalValue[index]"
-            :itemOptions="itemOptions"
-            :unitOptions="unitOptions"
-            :taxOptions="taxOptions"
-            :discountOptions="discountOptions"
-            :index="index"
-            :rowEmpty="(rowEmpty && index === 0) || false"
-            @deleteRow="(index) => removeRow(index)"
-            :errors="
-              !rowEmpty ? (Array.isArray(errors) ? errors[index] : null) : null
-            "
-          /> -->
           <div class="row q-col-gutter-md no-wrap">
             <div class="col-5">
-              <n-auto-complete
-                v-model="modalValue[index].item_id"
-                :options="itemOptions"
-                label="Item"
-                :error="
-                  !!errors?.[index]
-                    ? errors[index].item_id
-                      ? errors[index].item_id[0]
-                      : rowEmpty || false
-                    : false
-                "
-                :modal-component="ItemAdd"
-              />
+              <n-auto-complete v-model="modalValue[index].item_id" :options="itemOptions" label="Item" :error="!!errors?.[index]
+                ? errors[index].item_id
+                  ? errors[index].item_id[0]
+                  : rowEmpty || false
+                : false
+                " :modal-component="ItemAdd" @update:modelValue="onItemChange(index)" />
             </div>
             <div class="col-3">
-              <q-input
-                v-model.number="modalValue[index].quantity"
-                label="Quantity"
-                type="number"
-                :error-message="
-                  !!errors?.[index]
-                    ? errors[index].quantity
-                      ? errors[index].quantity[0]
-                      : false
-                    : false
-                "
-                :error="
-                  !!errors?.[index]
-                    ? errors[index].quantity
-                      ? true
-                      : false
-                    : false
-                "
-              ></q-input>
+              <q-input v-model.number="modalValue[index].quantity" label="Quantity" type="number" :error-message="!!errors?.[index]
+                ? errors[index].quantity
+                  ? errors[index].quantity[0]
+                  : false
+                : false
+                " :error="!!errors?.[index]
+    ? errors[index].quantity
+      ? true
+      : false
+    : false
+    "></q-input>
             </div>
             <div class="col-3">
-              <q-select
-                v-model="modalValue[index].unit_id"
-                :options="unitOptions"
-                label="Unit"
-                option-value="id"
-                option-label="name"
-                emit-value
-                map-options
-                :error-message="
-                  !!errors?.[index]
-                    ? errors[index].unit_id
-                      ? 'Please Select an Option'
-                      : false
+              <q-select v-model="modalValue[index].unit_id" :options="unitOptions" label="Unit" option-value="id"
+                option-label="name" emit-value map-options :error-message="!!errors?.[index]
+                  ? errors[index].unit_id
+                    ? 'Please Select an Option'
                     : false
-                "
-                :error="
-                  !!errors?.[index]
-                    ? errors[index].unit_id
-                      ? true
-                      : false
-                    : false
-                "
-              >
+                  : false
+                  " :error="!!errors?.[index]
+    ? errors[index].unit_id
+      ? true
+      : false
+    : false
+    ">
                 <template v-slot:append v-if="modalValue[index].unit_id">
-                  <q-icon
-                    name="close"
-                    size="xs"
-                    @click.stop.prevent="modalValue[index].unit_id = null"
-                    class="cursor-pointer"
-                  />
+                  <q-icon name="close" size="xs" @click.stop.prevent="modalValue[index].unit_id = null"
+                    class="cursor-pointer" />
                 </template>
               </q-select>
             </div>
-            <div
-              class="col-1 row no-wrap q-gutter-x-sm justify-center items-center"
-            >
-              <q-btn
-                flat
-                @click="() => removeRow(index)"
-                class="q-pa-sm"
-                color="transparent"
-              >
-                <q-icon
-                  name="delete"
-                  size="20px"
-                  color="negative"
-                  class="cursor-pointer"
-                ></q-icon>
+            <div class="col-1 row no-wrap q-gutter-x-sm justify-center items-center">
+              <q-btn flat @click="() => removeRow(index)" class="q-pa-sm" color="transparent">
+                <q-icon name="delete" size="20px" color="negative" class="cursor-pointer"></q-icon>
               </q-btn>
             </div>
           </div>
         </div>
         <div>
-          <q-btn @click="addRow" color="green" outline class="q-px-lg q-py-ms"
-            >Add Row</q-btn
-          >
+          <q-btn @click="addRow" color="green" outline class="q-px-lg q-py-ms">Add Row</q-btn>
         </div>
       </div>
     </q-card>
@@ -212,6 +153,12 @@ export default {
         )
       modalValue.value.splice(index, 1)
     }
+    const onItemChange = (index) => {
+      const itemIndex = props.itemOptions.findIndex((item) => item.id === modalValue.value[index].item_id)
+      if (itemIndex > -1) {
+        modalValue.value[index].unit_id = props.itemOptions[itemIndex].unit_id
+      }
+    }
     return {
       props,
       modalValue,
@@ -221,6 +168,7 @@ export default {
       // InvoiceRow,
       useCalcDiscount,
       rowEmpty,
+      onItemChange
     }
   },
 }
