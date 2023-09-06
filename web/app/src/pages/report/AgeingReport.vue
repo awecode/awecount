80c42274bd81577cd7a265f42d3a0a12fbcbb24b
Pramod <pramodthapa115@gmail.com>
2023-09-06 11:21:38 +0545

    wip: add ageing report

--- web/app/src/pages/report/AgeingReport.vue
+++ web/app/src/pages/report/AgeingReport.vue
@@ -3,9 +3,8 @@
         <div class="row q-gutter-x-md justify-end">
             <q-btn color="blue" label="Export XLS" icon-right="download" @click="onDownloadXls" />
         </div>
-        {{ reportData }}
-        <q-table title="Income Items" :rows="rows" :columns="newColumn" :loading="loading" :filter="searchQuery"
-            v-model:pagination="pagination" row-key="id" @request="onRequest" class="q-mt-md" :rows-per-page-options="[20]">
+        <q-table title="Income Items" :rows="reportData" :columns="newColumns" :loading="loading" row-key="id"
+            @request="onRequest" class="q-mt-md" hide-bottom>
             <template v-slot:top>
                 <div class="search-bar">
                     <!-- <q-input dense debounce="500" v-model="searchQuery" placeholder="Search" class="search-bar-wrapper">
@@ -14,72 +13,87 @@
                         </template>
                     </q-input> -->
                     <div class="flex items-end q-gutter-md">
-                        <DatePicker label="Date" v-model="filters.date"></DatePicker>
-                        <q-btn color="green" label="Filter" class="q-mr-md" @click="onFilterUpdate"></q-btn>
+                        <DatePicker label="Date" v-model="date"></DatePicker>
+                        <q-btn color="green" label="Filter" class="q-mr-md" @click="fetchData"></q-btn>
                     </div>
                 </div>
             </template>
-            <!-- <template v-slot:body-cell-status="props">
+            <template v-slot:body-cell-party="props">
                 <q-td :props="props">
-                    <div class="row align-center">
-                        <div class="text-white text-subtitle row items-center justify-center" :class="props.row.status == 'Issued'
-                            ? 'bg-blue-2 text-blue-10'
-                            : props.row.status == 'Resolved'
-                                ? 'bg-green-2 text-green-10'
-                                : props.row.status == 'Draft'
-                                    ? 'bg-orange-2 text-orange-10'
-                                    : 'bg-red-2 text-red-10'
-                            " style="border-radius: 8px; padding: 2px 10px">
-                            {{ props.row.status }}
-                        </div>
-                    </div>
+                    <router-link v-if="checkPermissions('CategoryModify')" style="font-weight: 500; text-decoration: none"
+                        class="text-blue" :to="`/parties/account/${props.row.party_id}/`">{{ props.row.party_name
+                        }}</router-link>
+                    <span v-else>{{ props.row.party_name }}</span>
                 </q-td>
             </template>
-
-            <template v-slot:body-cell-party_name="props">
-                <q-td :props="props">
-                    <div>
-                        <q-icon name="domain" size="sm" class="text-grey-8"></q-icon>
-                        <span class="text-capitalize q-ml-sm text-subtitle2 text-grey-8">{{
-                            props.row.party
-                        }}</span>
-                    </div>
-                </q-td>
-            </template>
-            <template v-slot:body-cell-actions="props">
-                <q-td :props="props">
-                    <div class="row q-gutter-x-md items-center">
-                        <q-btn color="blue" label="View" :to="`/debit-note/${props.row.id}/view/`"
-                            class="q-py-none q-px-md font-size-sm" style="font-size: 12px" />
-                    </div>
-                </q-td>
-            </template> -->
         </q-table>
     </div>
 </template>
 
 
 <script>
+import checkPermissions from 'src/composables/checkPermissions'
 export default {
-  setup() {
-    const reportData= ref(null)
-    const fields = ref({
-      start_date: null,
-      end_date: null,
-    })
-    const metaData = {
-      title: 'Collection Report | Awecount',
+    setup() {
+        const route = useRoute()
+        const reportData = ref([])
+        const loading = ref(false)
+        // const 
+        const date = ref(route.query.date || null)
+        const metaData = {
+            title: 'Customer Ageing Report | Awecount',
+        }
+        useMeta(metaData)
+        const fetchData = () => {
+            const endpoint = `/v1/report/ageing-report/?date=${date.value}`
+            loading.value = true
+            useApi(endpoint)
+                .then((data) => {
+                    reportData.value = data
+                    loading.value = false
+                })
+                .catch((err) => {
+                    console.log(err)
+                    loading.value = false
+                })
+            // TODO: add 404 error routing
+        }
+        const newColumns = [
+            {
+                name: 'party',
+                label: 'Party',
+                align: 'left',
+                field: 'party_name',
+            },
+            {
+                name: 'total',
+                label: 'Net Due',
+                align: 'left',
+                field: 'grand_total',
+            },
+            { name: 'total_30', label: '30 Days', align: 'left', field: 'total_30' },
+            { name: 'total_60', label: '60 Days', align: 'left', field: 'total_60' },
+            { name: 'total_90', label: '90 Days', align: 'left', field: 'total_90' },
+            { name: 'total_120', label: '120 Days', align: 'left', field: 'total_120' },
+            { name: 'total_120plus', label: '120 Days Plus', align: 'left', field: 'total_120plus' }
+        ]
+        return { reportData, fetchData, date, newColumns, checkPermissions }
+    },
+    created() {
+        if (!this.date) {
+            const now = new Date().toISOString().substring(0, 10)
+            const routeObj = {
+                path: this.$route.path,
+                query: {
+                    date: now
+                }
+            }
+            this.date = now
+            this.$router.push(routeObj)
+            // route.query
+        }
+        this.fetchData()
     }
-    useMeta(metaData)
-    const fetchData = () => {
-      const endpoint = `/v1/payment-receipt/collection-report/?start_date=${fields.value.start_date}&end_date=${fields.value.end_date}`
-      useApi(endpoint)
-        .then((data) => (reportData.value = data))
-        .catch((err) => console.log(err))
-      // TODO: add 404 error routing
-    }
-    return { reportData, fetchData, fields }
-  },
 }
 </script>
 
