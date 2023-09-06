3487c9d3dc94592c5d13ce8ec5e752afb6cd1728
bibek gautam <bibek@awecode.com>
2023-09-06 14:43:26 +0545

    feat: update salessettings options

--- server/django/apps/voucher/models/voucher_settings.py
+++ server/django/apps/voucher/models/voucher_settings.py
@@ -44,7 +44,8 @@ class SalesSetting(models.Model):
             'is_trade_discount_in_row': self.is_trade_discount_in_row,
             'enable_due_date_in_voucher': self.enable_due_date_in_voucher,
             'enable_import_challan': self.enable_import_challan,
-            'enable_amount_entry': self.enable_amount_entry
+            'enable_amount_entry': self.enable_amount_entry,
+            'show_rate_quantity_in_voucher': self.show_rate_quantity_in_voucher
         }
 
     def __str__(self):
