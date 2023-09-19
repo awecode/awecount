bdb7c11148c0a9f421ccd9a7aa0722edfe996b3e
Pramod <pramodthapa115@gmail.com>
2023-09-19 17:00:27 +0545

    feat: add table sort in customer list

--- web/app/src/pages/party/CustomerList.vue
+++ web/app/src/pages/party/CustomerList.vue
@@ -79,12 +79,14 @@ export default {
         label: 'Dr.',
         align: 'left',
         field: (row) => Math.round(row.dr * 100) / 100,
+        sortable: true
       },
       {
         name: 'cr',
         label: 'Cr.',
         align: 'left',
         field: (row) => Math.round(row.cr * 100) / 100,
+        sortable: true
       },
       {
         name: 'balance',
