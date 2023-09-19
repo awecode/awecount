460138d6608052702d48d32ce7fc24e6db85cf5a
bibek gautam <bibek@awecode.com>
2023-09-19 10:28:52 +0545

    chore: delete unwanted files from git index; update gitignore

--- server/django/temp.py
+++
@@ -1,5 +0,0 @@
-purchases = item.purchase_rows.order_by("id").values("quantity", "rate")
-purchases_cumsum = purchases.annotate(cumsum=Func(Sum('quantity'), template='%(expressions)s OVER (ORDER BY %(order_by)s)', order_by="id"))
-sales = item.sales_rows.order_by("id").values("quantity", "rate")
-sales_cumsum = sales.annotate(cumsum=Func(Sum('quantity'), template='%(expressions)s OVER (ORDER BY %(order_by)s)', order_by="id"))
-sold_qt = sales_cumsum.last()["cumsum"]
