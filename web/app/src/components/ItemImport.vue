4abbdd8940652516a2984d667f116beda42ff795
Pramod <pramodthapa115@gmail.com>
2023-09-21 13:28:42 +0545

    fix: minor fixes

--- web/app/src/components/ItemImport.vue
+++ web/app/src/components/ItemImport.vue
@@ -4,7 +4,8 @@
             Import xls from
         </div>
         <q-form @submit="() => onSubmit(file)" autofocus>
-            <q-input type="file" v-model="file" name="file"></q-input>
+            <q-file v-model="file" name="file" @update:model-value="
+                onFileChange(file, $event, 'front_image')"></q-file>
             <q-btn type="submit">Upload</q-btn>
         </q-form>
     </div>
@@ -13,11 +14,22 @@
 <script setup>
 const file = ref(null)
 const onSubmit = async (file) => {
-    // debugger
-    console.log(file[0])
     useApi('/v1/items/import/', {
         method: 'POST',
-        body: { file: file[0] },
+        body: { file: file.value },
     }).then(() => console.log('done')).catch((err) => console.log('error due to', err))
+}
+const onFileChange = (dct, event, attr) => {
+    const file = event
+    let reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.fileName = file.name
+    reader.onload = () => {
+        console.log(file)
+        file.value = reader.result
+    }
+    reader.onerror = function (error) {
+        console.error('Error: ', error)
+    }
 }
 </script>
