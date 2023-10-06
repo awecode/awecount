6c2e118d5477ba4d9ad4b93e89b79f11aaefbc24
Dipesh Acharya <dipesh@awecode.com>
2023-10-06 18:02:03 +0545

    fix: update page aggregate

--- server/django/awecount/libs/mixins.py
+++ server/django/awecount/libs/mixins.py
@@ -58,6 +58,8 @@ class TransactionsViewMixin(object):
         account_ids = self.get_account_ids(obj)
         start_date = param.get('start_date', None)
         end_date = param.get('end_date', None)
+        # page = int(param.get('page', 1))
+        # page_size = int(param.get('page_size', 20))
 
         account_id_list_str = ','.join([str(account_id) for account_id in account_ids])
 
@@ -119,34 +121,97 @@ class TransactionsViewMixin(object):
         """
 
         transactions = Transaction.objects.raw(raw_query)
+        opening_transaction = transactions
 
         aggregate = {}
         if start_date or end_date:
+            if start_date:
+                opening_transaction_query = f"""
+                     WITH AccountAggregation AS (
+                     SELECT
+                         je.id AS journal_entry_id,
+                         je.source_voucher_id,
+                         je.date,
+                         ct.model AS content_type_model,
+                         ct.app_label AS content_type_app_label,
+                         ARRAY_AGG(DISTINCT t.account_id) AS account_ids,
+                         STRING_AGG(DISTINCT acc.name, ',') AS account_names
+                     FROM
+                         ledger_transaction AS t
+                     JOIN
+                         ledger_journalentry AS je ON t.journal_entry_id = je.id
+                     JOIN
+                         ledger_account AS acc ON t.account_id = acc.id
+                     JOIN
+                         django_content_type AS ct ON je.content_type_id = ct.id
+                     WHERE
+                         t.account_id NOT IN ({account_id_list_str})
+                         {"AND je.date < '" + start_date + "'" if start_date else ''}
+                     GROUP BY
+                         je.id, je.source_voucher_id, je.date, ct.model, ct.app_label
+                 )
+
+                 SELECT
+                     SUM(t.id) as id,
+                     aa.source_voucher_id as source_id,
+                     aa.content_type_model as content_type_model,
+                     aa.content_type_app_label as content_type_app_label,
+                     SUM(t.dr_amount) AS total_dr_amount,
+                     SUM(t.cr_amount) AS total_cr_amount,
+                     aa.account_ids,
+                     aa.account_names,
+                     aa.date
+                 FROM
+                     ledger_transaction AS t
+                 JOIN
+                     AccountAggregation AS aa ON t.journal_entry_id = aa.journal_entry_id
+                 WHERE
+                     t.account_id IN ({account_id_list_str})
+                 GROUP BY
+                     aa.source_voucher_id,
+                     aa.date,
+                     aa.content_type_model,
+                     aa.content_type_app_label,
+                     aa.account_ids,
+                     aa.account_names
+                 ORDER BY
+                     aa.date DESC
+                     """
+                opening_transaction = Transaction.objects.raw(opening_transaction_query)
+            aggregate['opening'] = {
+                'dr': sum([t.total_dr_amount for t in opening_transaction if t.total_dr_amount is not None]),
+                'cr': sum([t.total_cr_amount for t in opening_transaction if t.total_cr_amount is not None])
+            }
+
             aggregate['total'] = {
-                'dr_amount': sum([t.total_dr_amount for t in transactions if t.total_dr_amount is not None]),
-                'cr_amount': sum([t.total_cr_amount for t in transactions if t.total_cr_amount is not None])
+                'dr': sum([t.total_dr_amount for t in transactions if t.total_dr_amount is not None]),
+                'cr': sum([t.total_cr_amount for t in transactions if t.total_cr_amount is not None])
             }
 
         page = self.paginate_queryset(transactions)
         serializer = TransactionEntrySerializer(page, many=True)
         data['transactions'] = self.paginator.get_response_data(serializer.data)
+        data['aggregate'] = aggregate
 
-        # paginated_aggregate
         limit = self.paginator.page_size
         offset = (self.paginator.page.number - 1) * limit
-        transactions_from_previous_pages = transactions[:offset]
-        data['page_aggregate'] = {
-            'total': {
-                'dr_amount': sum([t.total_dr_amount for t in page if t.total_dr_amount is not None]),
-                'cr_amount': sum([t.total_cr_amount for t in page if t.total_cr_amount is not None])
+        # transactions_from_previous_pages = transactions[:offset]
+        transactions_from_next_pages = transactions[offset + limit:]
+        data['page_cumulative'] = {
+            'current': {
+                'dr': sum([t.total_dr_amount for t in page if t.total_dr_amount is not None]),
+                'cr': sum([t.total_cr_amount for t in page if t.total_cr_amount is not None])
             },
-            'opening_balance': {
-                'dr_amount': sum(
-                    [t.total_dr_amount for t in transactions_from_previous_pages if t.total_dr_amount is not None]),
-                'cr_amount': sum(
-                    [t.total_cr_amount for t in transactions_from_previous_pages if t.total_cr_amount is not None])
+            # 'previous': {
+            #     'dr': sum(
+            #         [t.total_dr_amount for t in transactions_from_previous_pages if t.total_dr_amount is not None]),
+            #     'cr': sum(
+            #         [t.total_cr_amount for t in transactions_from_previous_pages if t.total_cr_amount is not None])
+            # },
+            'next': {
+                'dr': sum([t.total_dr_amount for t in transactions_from_next_pages if t.total_dr_amount is not None]),
+                'cr': sum([t.total_cr_amount for t in transactions_from_next_pages if t.total_cr_amount is not None])
             }
         }
 
-        data['aggregate'] = aggregate
         return Response(data)
