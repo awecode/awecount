5b2a6ce1673649cb7b82dbb09f7801de431b9f78
Pramod <pramodthapa115@gmail.com>
2023-10-06 18:07:29 +0545

    feat: add account list component

---
+++ web/app/src/components/account/AccountList.vue
@@ -0,0 +1,25 @@
+<template>
+    <!-- style="height: 40px; overflow-y: hidden;" -->
+    <div>
+        <div v-for="(account, index) in props.accounts" :key="account.id">
+            <router-link v-if="account.id !== accountId && showAllAccounts || index < 3" :to="`/account/${account.id}/view/`"
+                style="font-weight: 500; text-decoration: none" class="text-blue" :title="`${account.name}`">
+                {{ account.name }}
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+    accounts: {
+        type: Array,
+        required: true
+    },
+    accountId: {
+        type: Number,
+        required: true
+    }
+})
+const showAllAccounts = ref(false)
+</script>
