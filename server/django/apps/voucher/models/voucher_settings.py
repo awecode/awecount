bf81e581be25e2cbafd832579a449b5b6865f5d8
bibek gautam <bibek@awecode.com>
2023-10-11 10:45:30 +0545

    add new sales setting

--- server/django/apps/voucher/models/voucher_settings.py
+++ server/django/apps/voucher/models/voucher_settings.py
@@ -28,6 +28,7 @@ class SalesSetting(models.Model):
     show_rate_quantity_in_voucher = models.BooleanField(default=True)
     invoice_footer_text = models.CharField(max_length=255, null=True, blank=True)
     persist_pos_items = models.BooleanField(default=False)
+    enable_sales_date_edit = models.BooleanField(default=False)
 
     @property
     def fields(self):
