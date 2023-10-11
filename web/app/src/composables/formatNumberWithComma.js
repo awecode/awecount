62c30c24a14c22d6e93f02d2b1ec812683fa76db
Pramod <pramodthapa115@gmail.com>
2023-10-11 11:31:19 +0545

    fix: fix formatnumber with commacomposables

--- web/app/src/composables/formatNumberWithComma.js
+++ web/app/src/composables/formatNumberWithComma.js
@@ -1,26 +1,28 @@
 function formatNumberWithCommas(number) {
-    const roundNumber = Math.round(number * 100) / 100
-    // Convert the number to a string
-    const numStr = roundNumber.toString();
+  const roundNumber = Math.round(number * 100) / 100
+  // Convert the number to a string
+  const numStr = roundNumber.toString();
 
-    // Split the string into integer and decimal parts (if any)
-    const parts = numStr.split('.');
-    const intergers = parts[0].split('').reverse()
-    let tempGroup = ''
-    let commaCount = 0
-    const numbersArray = []
-    for (let i = 0; i <= intergers.length - 1; i++) {
-      tempGroup = tempGroup + intergers[i]
-      if (i === 2 && commaCount === 0) {
+  // Split the string into integer and decimal parts (if any)
+  const parts = numStr.split('.');
+  const integers = parts[0].split('').reverse()
+  let tempGroup = ''
+  let commaCount = 0
+  const numbersArray = []
+  for (let i = 0; i <= integers.length - 1; i++) {
+    tempGroup = tempGroup + integers[i]
+    if (i === 2 && commaCount === 0) {
+      numbersArray.push(tempGroup)
+      tempGroup = ''
+    } else if (i % 2 == 0 && i > 3) {
+      numbersArray.push(tempGroup)
+      tempGroup = ''
+    } else if (i == integers.length - 1) {
         numbersArray.push(tempGroup)
-        tempGroup = ''
-      } else if (i % 2 == 0 && i > 3) {
-        numbersArray.push(tempGroup)
-        tempGroup = ''
-      }
     }
-    // Join the groups with commas and return the formatted string
-    const totalString = numbersArray.join(',');
-    return totalString.split('').reverse().join('') + `${parts[1] ? '.'+ parts[1] : '' }`
   }
+  // Join the groups with commas and return the formatted string
+  const totalString = numbersArray.join(',');
+  return totalString.split('').reverse().join('') + `${parts[1] ? '.'+ parts[1] : '' }`
+}
 export default formatNumberWithCommas
