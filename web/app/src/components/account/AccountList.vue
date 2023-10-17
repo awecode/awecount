bbcf639fa104a403465f16ec153c1ad3f7e329b9
Pramod <pramodthapa115@gmail.com>
2023-10-17 15:19:55 +0545

    feat: add accounts list open and close in transaction table

--- web/app/src/components/account/AccountList.vue
+++ web/app/src/components/account/AccountList.vue
@@ -1,20 +1,34 @@
 <template>
-    <!-- style="height: 40px; overflow-y: hidden;" -->
-    <div>
-        <div v-for="(account, index) in props.accounts" :key="index">
-            <router-link :to="`/account/${account.id}/view/`" style="font-weight: 500; text-decoration: none"
-                class="text-blue block" :title="`${account.name}`">
-                {{ account.name }}
-            </router-link>
-        </div>
+  <!-- style="height: 40px; overflow-y: hidden;" -->
+  <div v-if="props.accounts.length > 2">
+    <div class="transition" :class="`${openState ? '': 'max-h-[40px]'} overflow-y-hidden`">
+      <div v-for="(account, index) in props.accounts" :key="index">
+        <router-link :to="`/account/${account.id}/view/`" style="font-weight: 500; text-decoration: none"
+          class="text-blue block" :title="`${account.name}`">
+          {{ account.name }}
+        </router-link>
+      </div>
     </div>
+    <div class="hover:bg-gray-200 text-center" @click="openState = !openState">
+      <q-icon size="sm" color="blue" name="mdi-chevron-down" class="transition-transform" :class="openState ? 'rotate-180' : ''"></q-icon>
+    </div>
+  </div>
+  <div v-else>
+    <div v-for="(account, index) in props.accounts" :key="index">
+      <router-link :to="`/account/${account.id}/view/`" style="font-weight: 500; text-decoration: none"
+        class="text-blue block" :title="`${account.name}`">
+        {{ account.name }}
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 const props = defineProps({
-    accounts: {
-        type: Array,
-        required: true
-    }
+  accounts: {
+    type: Array,
+    required: true
+  }
 })
+const openState = ref(false)
 </script>
