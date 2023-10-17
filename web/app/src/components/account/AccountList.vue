6dc1751ae0dc9e9d0ee337de6d3963f002c9b55a
Pramod <pramodthapa115@gmail.com>
2023-10-17 15:29:58 +0545

    feat: show hidden accounts in tarsaction table in print

--- web/app/src/components/account/AccountList.vue
+++ web/app/src/components/account/AccountList.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- style="height: 40px; overflow-y: hidden;" -->
   <div v-if="props.accounts.length > 2">
-    <div class="transition" :class="`${openState ? '': 'max-h-[40px]'} overflow-y-hidden`">
+    <div class="transition" :class="`${openState ? '': 'max-h-[40px]'} overflow-y-hidden accounts-con`">
       <div v-for="(account, index) in props.accounts" :key="index">
         <router-link :to="`/account/${account.id}/view/`" style="font-weight: 500; text-decoration: none"
           class="text-blue block" :title="`${account.name}`">
@@ -32,3 +32,11 @@ const props = defineProps({
 })
 const openState = ref(false)
 </script>
+
+<style scoped>
+@media print {
+  .accounts-con {
+    max-height: none !important;
+  }
+}
+</style>
