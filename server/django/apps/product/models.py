4b1fc4298854f906eaa9a7ebf594f580994ae4df
bibek gautam <bibek@awecode.com>
2023-10-30 10:38:47 +0545

    feat: make code field nullable in Item model

--- server/django/apps/product/models.py
+++ server/django/apps/product/models.py
@@ -443,7 +443,7 @@ def set_inventory_transactions(model, date, *args, clear=True):
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
-    code = models.CharField(max_length=50)
+    code = models.CharField(max_length=50, blank=True, null=True)
     unit = models.ForeignKey(Unit, blank=True, null=True, on_delete=models.SET_NULL)
     category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL, related_name='items')
     description = models.TextField(blank=True, null=True)
