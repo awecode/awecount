1df1da1cc660f6741bec4123f200a92779176a97
Pramod <pramodthapa115@gmail.com>
2023-12-04 10:39:43 +0545

    feat: create item merge page

---
+++ web/app/src/pages/settings/ItemMerge.vue
@@ -0,0 +1,121 @@
+<template>
+  <div>
+    Item merge
+  </div>
+</template>
+
+<script setup>
+
+const itemOptions = [
+  {
+    'id': 486,
+    'name': 'Dwandwa ko Awasan',
+    'unit_id': 1,
+    'rate': 260,
+    'tax_scheme_id': 2,
+    'code': '9789937918633',
+    'description': null,
+    'is_trackable': true
+  },
+  {
+    'id': 6,
+    'name': 'Radha',
+    'unit_id': 1,
+    'rate': 500,
+    'tax_scheme_id': 2,
+    'code': '9789937541275',
+    'description': null,
+    'is_trackable': true
+  },
+  {
+    'id': 407,
+    'name': 'The Fountainhead',
+    'unit_id': 1,
+    'rate': 640,
+    'tax_scheme_id': 2,
+    'code': '9780451191151',
+    'description': null,
+    'is_trackable': true
+  },
+  {
+    'id': 524,
+    'name': 'Prometheus',
+    'unit_id': 1,
+    'rate': 205,
+    'tax_scheme_id': 2,
+    'code': '9789993326403',
+    'description': null,
+    'is_trackable': true
+  },
+  {
+    'id': 433,
+    'name': "Man's Search for Meaning",
+    'unit_id': 1,
+    'rate': 480,
+    'tax_scheme_id': 2,
+    'code': '9781846041242',
+    'description': null,
+    'is_trackable': true
+  },
+  {
+    'id': 1587,
+    'name': 'Chanakya Neeti',
+    'unit_id': 1,
+    'rate': 225,
+    'tax_scheme_id': 2,
+    'code': '9789937044158',
+    'description': null,
+    'is_trackable': true
+  },
+  {
+    'id': 437,
+    'name': 'Selected Short Stories',
+    'unit_id': 1,
+    'rate': 500,
+    'tax_scheme_id': 2,
+    'code': '9781792998393',
+    'description': null,
+    'is_trackable': true
+  },
+  {
+    'id': 543,
+    'name': 'Purpala',
+    'unit_id': 1,
+    'rate': 315,
+    'tax_scheme_id': 2,
+    'code': '9789937910132',
+    'description': null,
+    'is_trackable': true
+  },
+  {
+    'id': 1156,
+    'name': 'Maharani',
+    'unit_id': 1,
+    'rate': 450,
+    'tax_scheme_id': 2,
+    'code': '9789937934459',
+    'description': null,
+    'is_trackable': true
+  },
+  {
+    'id': 1620,
+    'name': 'Hindu Sanskritiko Parisheelan',
+    'unit_id': 1,
+    'rate': 418,
+    'tax_scheme_id': 2,
+    'code': 'hsp_mmd',
+    'description': null,
+    'is_trackable': true
+  },
+  {
+    'id': 461,
+    'name': 'Social Science',
+    'unit_id': null,
+    'rate': 950,
+    'tax_scheme_id': null,
+    'code': 'ss_xyz',
+    'description': null,
+    'is_trackable': true
+  }
+]
+</script>
