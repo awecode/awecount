943ed4b7093836e5530e9c8f8cef3d13a25c3a37
bibek-beebayk <beebayk0001@gmail.com>
2023-12-29 13:02:30 +0545

    feat: fifo update on credit note cteation;

--- server/django/apps/voucher/serializers/debit_note.py
+++ server/django/apps/voucher/serializers/debit_note.py
@@ -71,9 +71,47 @@ class DebitNoteCreateSerializer(StatusReversionMixin, DiscountObjectTypeSerializ
             if not row_serializer.is_valid():
                 raise serializers.ValidationError(row_serializer.errors)
         return rows
+    
+    def cancel_purchase(self, instance, rows_data):
+        purchase_vouchers = instance.invoices.all()
+        for voucher in purchase_vouchers:
+            purchase_rows = voucher.rows.all()
+            for row in purchase_rows:
+                row_data = next((item for item in rows_data if item['id'] == row.id), None)
+                row.quantity -= row_data["quantity"]
+                # row.remaining_quantity -= row_data["quantity"]
+                row.save()
+                # sales_rows = SalesVoucherRow.objects.filter(sold_items__has_key=str(row.id))
+                # for sales_row in sales_rows:
+                #     sold_qt = sales_row.sold_items.get(str(row.id))
+                #     if sold_qt 
+                # import ipdb; ipdb.set_trace()
+                # sales_row_id = rows_data[row.id]
+                # sold_items = row.sold_items
+                # purchase_row_ids = [key for key, value in sold_items.items()]
+                # purchase_voucher_rows = PurchaseVoucherRow.objects.filter(id__in=purchase_row_ids).order_by("voucher__date", "id")
+                # quantity = row_data["quantity"]
+                # for purchase_row in purchase_voucher_rows:
+                #     can_be_added = purchase_row.quantity - purchase_row.remaining_quantity
+                #     diff = quantity - purchase_row.quantity
+                #     if diff <= can_be_added:
+                #         purchase_row.remaining_quantity += quantity
+                #         purchase_row.save()
+                #         row.sold_items[str(purchase_row.id)] -= quantity
+                #         row.save()
+                #         break
+                #     else:
+                #         purchase_row.remaining_quantity += can_be_added
+                #         purchase_row.save()
+                #         quantity -= can_be_added
+                #         row.sold_items.pop(str(purchase_row.id))
+                #         row.save()
+                #         continue
 
     def create(self, validated_data):
+        from copy import deepcopy
         rows_data = validated_data.pop('rows')
+        rows_data_copy = deepcopy(rows_data)
         invoices = validated_data.pop('invoices')
         request = self.context['request']
         self.assign_fiscal_year(validated_data)
@@ -84,11 +122,14 @@ class DebitNoteCreateSerializer(StatusReversionMixin, DiscountObjectTypeSerializ
         validated_data['user_id'] = request.user.id
         instance = DebitNote.objects.create(**validated_data)
         for index, row in enumerate(rows_data):
+            row.pop("id")
             row = self.assign_discount_obj(row)
             DebitNoteRow.objects.create(voucher=instance, **row)
         instance.invoices.clear()
         instance.invoices.add(*invoices)
         instance.apply_transactions()
+        if self.context["request"].company.inventory_setting.enable_fifo:
+            self.cancel_purchase(instance, rows_data_copy)
         return instance
 
     def update(self, instance, validated_data):
