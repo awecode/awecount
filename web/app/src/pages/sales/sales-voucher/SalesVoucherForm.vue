69025479d9379863f27fbb00e54e875fa8dbf0b2
Pramod <pramodthapa115@gmail.com>
2024-01-05 16:58:56 +0545

    feat: add client side validation in challans add in sales voucher form

--- web/app/src/pages/sales/sales-voucher/SalesVoucherForm.vue
+++ web/app/src/pages/sales/sales-voucher/SalesVoucherForm.vue
@@ -16,7 +16,7 @@
               <div v-if="fields.challans && fields.challans.length > 0">
                 <q-input dense v-model="fields.challan_numbers" disable label="Import challan(s)"></q-input>
               </div>
-              <q-dialog v-model="importChallanModal">
+              <q-dialog v-model="importChallanModal" @before-hide="errors && delete errors?.fiscal_year && delete errors?.invoice_no">
                 <q-card style="min-width: min(60vw, 400px)">
                   <q-card-section class="bg-grey-4">
                     <div class="text-h6">
@@ -25,10 +25,10 @@
                   </q-card-section>
 
                   <q-card-section class="q-mx-lg">
-                    <q-input v-model.number="referenceFormData.invoice_no" label="Challan No.*" autofocus type="number"></q-input>
+                    <q-input v-model.number="referenceFormData.invoice_no" label="Challan No.*" autofocus type="number" :error="!!errors?.invoice_no" :error-message="errors?.invoice_no"></q-input>
                     <q-select class="q-mt-md" label="Fiscal Year" v-model="referenceFormData.fiscal_year"
                       :options="formDefaults.options.fiscal_years" option-value="id" option-label="name" map-options
-                      emit-value></q-select>
+                      emit-value :error="!!errors?.fiscal_year" :error-message="errors?.fiscal_year" ></q-select>
                     <div class="row justify-end q-mt-lg">
                       <q-btn color="green" label="Add" size="md" @click="fetchInvoice(fields)"></q-btn>
                     </div>
@@ -39,8 +39,8 @@
             <div class="col-md-6 col-12">
               <div class="row">
                 <div class="col-10">
-                  <q-input v-model="fields.customer_name" label="Customer Name" :error-message="errors.customer_name"
-                    :error="!!errors.customer_name" v-if="partyMode && fields.mode !== 'Credit'">
+                  <q-input v-model="fields.customer_name" label="Customer Name" :error-message="errors?.customer_name"
+                    :error="!!errors?.customer_name" v-if="partyMode && fields.mode !== 'Credit'">
                   </q-input>
                   <n-auto-complete v-else v-model="fields.party" :options="formDefaults.collections?.parties"
                     label="Party" :error="errors?.party ? errors?.party : null" :modal-component="checkPermissions('PartyCreate') ? PartyForm : null"
@@ -58,8 +58,8 @@
               v-model="fields.date" class="col-md-6 col-12" :error="!!errors?.date"
               :error-message="errors?.date"></date-picker>
             <DateInputDisabled v-else :date="fields.date" class="col-md-6 col-12" label="Invoice Date*" />
-            <q-input v-model="fields.address" class="col-md-6 col-12" label="Address" :error-message="errors.address"
-              :error="!!errors.address"></q-input>
+            <q-input v-model="fields.address" class="col-md-6 col-12" label="Address" :error-message="errors?.address"
+              :error="!!errors?.address"></q-input>
             <date-picker v-if="formDefaults.options?.enable_due_date_in_voucher" label="Due Date"
               v-model="fields.due_date" class="col-md-6 col-12" :error="!!errors?.due_date"
               :error-message="errors?.due_date" :toLimit="fields.date"></date-picker>
@@ -81,7 +81,7 @@
     fields.discount_type === 'Percent'
     ">
                   <q-input class="col-6" v-model.number="fields.discount" label="Discount"
-                    :error-message="errors.discount" :error="!!errors.discount"></q-input>
+                    :error-message="errors?.discount" :error="!!errors?.discount"></q-input>
                 </div>
                 <div class="col-3 row" v-if="formDefaults.options?.show_trade_discount_in_voucher &&
                   ['Percent', 'Amount'].includes(fields.discount_type)
@@ -93,8 +93,8 @@
           </div>
           <!-- <div class="row q-col-gutter-md"></div> -->
           <div class="row q-col-gutter-md">
-            <q-select v-model="fields.mode" label="Mode *" class="col-12 col-md-6" :error-message="errors.mode"
-              :error="!!errors.mode" :options="staticOptions.modes.concat(
+            <q-select v-model="fields.mode" label="Mode *" class="col-12 col-md-6" :error-message="errors?.mode"
+              :error="!!errors?.mode" :options="staticOptions.modes.concat(
                 formDefaults.collections?.bank_accounts
               )
                 " option-value="id" option-label="name" map-options emit-value>
@@ -109,7 +109,7 @@
     " :discountOptions="discountOptionsComputed" :taxOptions="formDefaults.collections?.tax_schemes" v-model="fields.rows" :mainDiscount="{
     discount_type: fields.discount_type,
     discount: fields.discount,
-  }" :errors="!!errors.rows ? errors.rows : null" @deleteRowErr="(index, deleteObj) => deleteRowErr(index, errors, deleteObj)
+  }" :errors="!!errors?.rows ? errors.rows : null" @deleteRowErr="(index, deleteObj) => deleteRowErr(index, errors, deleteObj)
   " :enableRowDescription="formDefaults.options?.enable_row_description"
         :showRowTradeDiscount="formDefaults.options?.show_trade_discount_in_row" :inputAmount="formDefaults.options?.enable_amount_entry" :showRateQuantity="formDefaults.options?.show_rate_quantity_in_voucher"
       :hasChallan="!!(fields.challans && fields.challans.length > 0)"  ></invoice-table>
@@ -238,6 +238,7 @@ export default {
             `?invoice_no=${referenceFormData.value.invoice_no}&fiscal_year=${referenceFormData.value.fiscal_year}`
           )
             .then((data) => {
+              formData.errors.value = {}
               const response = { ...data }
               if (fields.challans) {
                 if (fields.party && fields.party !== response.party || fields.customer_name && fields.customer_name !== response.customer_name) {
@@ -297,12 +298,16 @@ export default {
               importChallanModal.value = false
             })
             .catch((err) => {
+              let message
+              if (err.status === 404) message = 'Invoice Not Found!'
+              else message = err.data?.detail || 'Server Error! Please contact us with the problem.'
               $q.notify({
-                  color: 'red-6',
-                  message: err.data?.detail || 'Error',
-                  icon: 'report_problem',
-                  position: 'top-right',
-                })
+                color: 'red-6',
+                message: message || 'Error',
+                icon: 'report_problem',
+                position: 'top-right',
+              })
+              formData.errors.value = {}
             })
         }
       } else {
@@ -312,6 +317,13 @@ export default {
           icon: 'report_problem',
           position: 'top-right',
         })
+        if (!formData?.errors?.value) formData.errors.value = {}
+        if (!referenceFormData.value.invoice_no) {
+          formData.errors.value.invoice_no = "Invoice Number is required!"
+        }
+        if (!referenceFormData.value.fiscal_year) {
+          formData.errors.value.fiscal_year = "Fiscal Year is required!"
+        }
       }
     }
     const onPartyChange = (value) => {
