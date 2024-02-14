6b09667a7df88863c13dc0445cc8e9af4ef70dd6
Pramod <pramodthapa115@gmail.com>
2024-02-14 12:31:23 +0545

    fix: minor fix

--- web/app/src/boot/sentry.js
+++ web/app/src/boot/sentry.js
@@ -5,8 +5,8 @@ import * as Sentry from '@sentry/vue';
 export default boot(({ app }) => {
   Sentry.init({
     app,
-    // dsn: 'https://51ed8b7e9bb19758cd85c6b792f9c9c8@o374601.ingest.sentry.io/4506143333154816' for local test
-    dsn: 'https://a33dd63776e1f24aea26b1a39a0aebf3@o4506710210641920.ingest.sentry.io/4506710212345856',
+    dsn: 'https://51ed8b7e9bb19758cd85c6b792f9c9c8@o374601.ingest.sentry.io/4506143333154816',
+    // dsn: 'https://a33dd63776e1f24aea26b1a39a0aebf3@o4506710210641920.ingest.sentry.io/4506710212345856',
     enabled: process.env.NODE_ENV !== 'development',
     integrations: [],
     trackComponents: true,
