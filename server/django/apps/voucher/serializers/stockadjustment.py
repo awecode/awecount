b450c856f7af80b23276896b29c042a926b46d36
Samrityy <smritymulmi23@gmail.com>
2024-02-23 17:30:53 +0545

    feat:add StockAdjustmentVoucherRowSerializer

--- server/django/apps/voucher/serializers/stockadjustment.py
+++ server/django/apps/voucher/serializers/stockadjustment.py
@@ -4,34 +4,61 @@ from apps.voucher.models import StockAdjustmentVoucher
 from awecount.libs import get_next_voucher_no
 
 
+class StockAdjustmentVoucherRowSerializer(serializers.ModelSerializer):
+    item_id = serializers.IntegerField(required=True)
+    unit_id = serializers.IntegerField(required=True)
+
+    class Meta:
+        model = StockAdjustmentVoucher
+        exclude = (
+            "items",
+            "voucher",
+            "unit",
+        )
+
+
+class StockAdjustmentVoucherRowDetailSerializer(serializers.ModelSerializer):
+    item = serializers.CharField(source="item.name")
+    unit = serializers.CharField(source="unit.name")
+
+    class Meta:
+        model = StockAdjustmentVoucher
+        exclude = ("items", "voucher", "unit")
+
+
 class StockAdjustmentVoucherCreateSerializer(serializers.ModelSerializer):
-    voucher_no=serializers.ReadOnlyField()
-    
+    voucher_no = serializers.ReadOnlyField()
+    rows = StockAdjustmentVoucherRowSerializer(many=True)
 
     def assign_voucher_number(self, validated_data, instance):
-            
-            if instance and instance.voucher_no:
-                return
-            if validated_data.get('status') in ['Cancelled']:
-                return
-            next_voucher_no = get_next_voucher_no(StockAdjustmentVoucher, self.context['request'].company_id)
-            validated_data['voucher_no'] = next_voucher_no
+        if instance and instance.voucher_no:
+            return
+        if validated_data.get("status") in ["Cancelled"]:
+            return
+        next_voucher_no = get_next_voucher_no(
+            StockAdjustmentVoucher, self.context["request"].company_id
+        )
+        validated_data["voucher_no"] = next_voucher_no
 
-    def create(self,validated_data):
+    def create(self, validated_data):
         self.assign_voucher_number(validated_data, instance=None)
         instance = StockAdjustmentVoucher.objects.create(**validated_data)
         return instance
-    
+
     class Meta:
         model = StockAdjustmentVoucher
-        fields =['id','voucher_no','date','status','purpose','remarks']
+        exclude = ("company",)
+
 
 class StockAdjustmentVoucherListSerializer(serializers.ModelSerializer):
     class Meta:
         model = StockAdjustmentVoucher
-        fields = ['id','voucher_no', 'date','status','purpose','remarks']
+        exclude = ("company",)
+
 
 class StockAdjustmentVoucherDetailSerializer(serializers.ModelSerializer):
+    row = StockAdjustmentVoucherRowDetailSerializer(many=True)
+
     class Meta:
         model = StockAdjustmentVoucher
-        fields = ['id','voucher_no', 'date','status','purpose','remarks']
+        exclude = ("company",)
