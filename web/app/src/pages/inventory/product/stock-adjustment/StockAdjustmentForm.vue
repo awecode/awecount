ca6c3b088138e54ff97cff0c1132f5b2766bff97
Pramod <pramodthapa115@gmail.com>
2024-02-26 15:48:21 +0545

    feat: add form submit on stock adjustment form

--- web/app/src/pages/inventory/product/stock-adjustment/StockAdjustmentForm.vue
+++ web/app/src/pages/inventory/product/stock-adjustment/StockAdjustmentForm.vue
@@ -19,7 +19,8 @@
           </div>
           <div class="q-mt-lg">
             <AdjustmentInvoiceTable v-model="fields.rows" :itemOptions="formDefaults?.collections?.items"
-              :unitOptions="formDefaults?.collections?.units" :errors="errors?.rows" @deleteRowErr="(index) => deleteRowErr(index, errors)">
+              :unitOptions="formDefaults?.collections?.units" :errors="errors?.rows"
+              @deleteRowErr="(index) => deleteRowErr(index, errors)">
             </AdjustmentInvoiceTable>
           </div>
           <div class="q-mt-lg">
@@ -28,10 +29,10 @@
           </div>
         </q-card-section>
         <div class="text-right q-pr-md q-pb-lg">
-          <q-btn v-if="checkPermissions('BrandModify') && isEdit" :loading="loading" @click.prevent="submitForm"
-            color="green" label="Update" class="q-ml-auto" type="submit" />
-          <q-btn v-if="!isEdit && checkPermissions('BrandCreate')" :loading="loading" @click.prevent="submitForm"
-            color="green" label="Create" class="q-ml-auto" type="submit" />
+          <q-btn v-if="checkPermissions('BrandModify') && isEdit" :loading="loading"
+            @click.prevent="onSubmitClick(fields.status)" color="green" label="Update" class="q-ml-auto" type="submit" />
+          <q-btn v-if="!isEdit && checkPermissions('BrandCreate')" :loading="loading"
+            @click.prevent="onSubmitClick('Issued')" color="green" label="Create" class="q-ml-auto" type="submit" />
         </div>
       </q-card>
     </q-card>
@@ -48,6 +49,7 @@ export default {
     const metaData = {
       title: 'Stock Adjustment | Awecount',
     }
+    useMeta(metaData)
     const purposeChoices = [
       {
         label: 'Stock In',
@@ -70,9 +72,15 @@ export default {
     const deleteRowErr = (index, errors) => {
       if (!!errors?.rows) errors.rows.splice(index, 1)
     }
-    useMeta(metaData)
+    const onSubmitClick = async (status) => {
+      const originalStatus = formData.fields.value.status
+      formData.fields.value.status = status
+      try { await formData.submitForm() } catch (err) {
+        formData.fields.value.status = originalStatus
+      }
+    }
     return {
-      ...formData, checkPermissions, purposeChoices, deleteRowErr
+      ...formData, checkPermissions, purposeChoices, deleteRowErr, onSubmitClick
     }
   },
 }
