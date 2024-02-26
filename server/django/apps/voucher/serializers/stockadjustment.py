3f3b0e90c83cf29cd8b9f7e140e96b3ed60b0780
Pramod <pramodthapa115@gmail.com>
2024-02-26 18:17:35 +0545

    fix: fix in StockAdjustmentVoucher serializers; feat: add update method in StockAdjustmentVoucherCreateSerializer

--- server/django/apps/voucher/serializers/stockadjustment.py
+++ server/django/apps/voucher/serializers/stockadjustment.py
@@ -5,27 +5,19 @@ from awecount.libs import get_next_voucher_no
 
 
 class StockAdjustmentVoucherRowSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
     item_id = serializers.IntegerField(required=True)
     unit_id = serializers.IntegerField(required=True)
-
+    item_name = serializers.ReadOnlyField(source="item.name")
+    unit_name = serializers.ReadOnlyField(source="unit.name")
+    
     class Meta:
         model = StockAdjustmentVoucherRow
         exclude = (
             "item",
             "voucher",
-            # "unit",
+            "unit",
         )
-        
-        
-
-
-class StockAdjustmentVoucherRowDetailSerializer(serializers.ModelSerializer):
-    item = serializers.CharField(source="item.name")
-    unit = serializers.CharField(source="unit.name")
-
-    class Meta:
-        model = StockAdjustmentVoucherRow
-        exclude = ("items", "voucher", "unit")
 
 
 class StockAdjustmentVoucherCreateSerializer(serializers.ModelSerializer):
@@ -41,10 +33,33 @@ class StockAdjustmentVoucherCreateSerializer(serializers.ModelSerializer):
             StockAdjustmentVoucher, self.context["request"].company_id
         )
         validated_data["voucher_no"] = next_voucher_no
+    
+    # def validate_rows(self, rows):
+    #     import ipdb; ipdb.set_trace()
+    #     for row in rows:
+    #         row_serializer = StockAdjustmentVoucherRowSerializer(data=row)
+    #         if not row_serializer.is_valid():
+    #             raise serializers.ValidationError(row_serializer.errors)
+    #     return rows
 
     def create(self, validated_data):
+        rows_data = validated_data.pop("rows")
         self.assign_voucher_number(validated_data, instance=None)
         instance = StockAdjustmentVoucher.objects.create(**validated_data)
+        for index, row in enumerate(rows_data):
+            if row.get("id"):
+                row.pop("id")
+            StockAdjustmentVoucherRow.objects.create(voucher=instance, **row)
+        return instance
+
+    def update(self, instance, validated_data):
+        rows_data = validated_data.pop("rows")
+        StockAdjustmentVoucher.objects.filter(pk=instance.id).update(**validated_data)
+        for index, row in enumerate(rows_data):
+            # import ipdb; ipdb.set_trace();
+            StockAdjustmentVoucherRow.objects.update_or_create(
+                voucher=instance, pk=row.get("id"), defaults=row
+            )
         return instance
 
     class Meta:
@@ -59,7 +74,7 @@ class StockAdjustmentVoucherListSerializer(serializers.ModelSerializer):
 
 
 class StockAdjustmentVoucherDetailSerializer(serializers.ModelSerializer):
-    row = StockAdjustmentVoucherRowDetailSerializer(many=True)
+    rows = StockAdjustmentVoucherRowSerializer(many=True)
 
     class Meta:
         model = StockAdjustmentVoucher
