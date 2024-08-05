78bc7bc29eab41b30aed7c2acd1cce221634a905
Pramod-115 <pramodthapa115@gmail.com>
2024-08-05 18:10:24 +0545

    feat: add select-pagination in purchase order form

--- web/app/src/pages/purchase/purchase-order/PurchaseOrderForm.vue
+++ web/app/src/pages/purchase/purchase-order/PurchaseOrderForm.vue
@@ -12,7 +12,8 @@
         <q-card-section>
           <div class="row q-col-gutter-md">
             <div class="col-md-6 col-12">
-              <n-auto-complete v-model="fields.party" :options="formDefaults.collections?.parties" label="Party *"
+              <n-auto-complete-v2 v-model="fields.party" :options="formDefaults.collections?.parties" label="Party *"
+                endpoint="/v1/purchase-order/create-defaults/parties" :staticOption="fields.selected_party_obj"
                 :error="errors?.party ? errors?.party : null"
                 :modal-component="checkPermissions('PartyCreate') ? PartyForm : null" />
               <div></div>
@@ -22,8 +23,8 @@
           </div>
         </q-card-section>
       </q-card>
-      <ChallanTable :itemOptions="formDefaults.collections ? formDefaults.collections.items : null
-        " :unitOptions="formDefaults.collections ? formDefaults.collections.units : null
+      <ChallanTable v-if="formDefaults.collections" :itemOptions="formDefaults.collections ? formDefaults.collections.items : null
+        " usedIn="purchase-order" :unitOptions="formDefaults.collections ? formDefaults.collections.units : null
     " v-model="fields.rows" :errors="!!errors.rows ? errors.rows : null"
         @deleteRow="(index, deleteObj) => deleteRow(index, errors, deleteObj)" :isEdit="isEdit"></ChallanTable>
       <div class="q-px-md">
@@ -154,14 +155,13 @@ export default {
       (newValue) => {
         if (newValue) {
           const index =
-            formData.formDefaults.value.collections?.parties.findIndex(
+            formData.formDefaults.value.collections?.parties.results.findIndex(
               (option) => option.id === newValue
             )
           if (index > -1) {
             formData.fields.value.address =
-              formData.formDefaults.value.collections.parties[index].address
+              formData.formDefaults.value.collections.parties.results[index].address
           }
-          // const index = formDefaults.
         }
       }
     )
