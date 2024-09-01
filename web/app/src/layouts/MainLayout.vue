d2210c90ce921ae5a1e4386ede8dd0bc49a6db84
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2024-09-01 16:39:37 +0545

    fix: hide inventory conversion and bill of material

--- web/app/src/layouts/MainLayout.vue
+++ web/app/src/layouts/MainLayout.vue
@@ -23,7 +23,7 @@
               <q-tooltip :delay="1000" :offset="[0, 10]">Fiscal Yaar</q-tooltip></q-btn>
             <q-btn v-if="store.companyInfo?.config_template === 'np'" class="dateSwitcher bg-grey-7 text-grey-2"
               @click="store.isCalendarInAD = !store.isCalendarInAD">{{
-          activeDateFormat }}
+                activeDateFormat }}
               <q-tooltip :delay="1000" :offset="[0, 10]">Change Date Format</q-tooltip>
             </q-btn>
             <a target="_blank" href="https://docs.awecount.com/" style="color: inherit;">
@@ -242,18 +242,18 @@ const essentialLinks: EssentialLinkProps[] = [
         link: '/items/inventory-adjustment/list',
         hide: !checkPermissions('InventoryAdjustmentVoucherView')
       },
-      {
-        title: 'Bill of Material',
-        icon: 'mdi-receipt',
-        link: '/items/bill-of-material/list',
-        hide: !checkPermissions('BillOfMaterialView')
-      },
-      {
-        title: 'Inventory Conversion',
-        icon: 'mdi-shuffle-variant',
-        link: '/items/inventory-conversion/list',
-        hide: !checkPermissions('InventoryConversionVoucherView')
-      },
+      // {
+      //   title: 'Bill of Material',
+      //   icon: 'mdi-receipt',
+      //   link: '/items/bill-of-material/list',
+      //   hide: !checkPermissions('BillOfMaterialView')
+      // },
+      // {
+      //   title: 'Inventory Conversion',
+      //   icon: 'mdi-shuffle-variant',
+      //   link: '/items/inventory-conversion/list',
+      //   hide: !checkPermissions('InventoryConversionVoucherView')
+      // },
     ],
   },
   // {
