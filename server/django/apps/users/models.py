ac02c09055a6b7c9c1569c27422e9b264af5596c
Pramod-115 <pramodthapa115@gmail.com>
2024-09-26 12:11:51 +0545

    feat: add third invoice template

--- server/django/apps/users/models.py
+++ server/django/apps/users/models.py
@@ -43,7 +43,7 @@ class Company(models.Model):
     TEMPLATE_CHOICES = [
         (1, "Template 1"),
         (2, "Template 2"),
-        # (3, "Template 3"),
+        (3, "Template 3"),
         # (4, "Template 4")
     ]
 
