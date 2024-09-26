0b0c18503c62d720d3be35fe64f408884604463d
Rohan Dhimal <drowhann@gmail.com>
2024-09-26 11:06:59 +0545

    feat: add viewset to list item with it's inventory account's current balance

---
+++ server/django/apps/product/api/partner.py
@@ -0,0 +1,15 @@
+from rest_framework.viewsets import ReadOnlyModelViewSet
+
+from apps.product.models import Item
+from apps.product.serializers.partner import PartnerItemSerializer
+
+
+class PartnerItemViewSet(ReadOnlyModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = PartnerItemSerializer
+    pagination_class = None
+
+    def get_queryset(self):
+        return self.queryset.filter(company_id=self.request.company_id,
+                                        account__isnull=False, code__isnull=False
+                                    ).prefetch_related('account')
