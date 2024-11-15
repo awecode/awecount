9d31252169f872b19bc9e3bc12c0a2d9fe55e3ac
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2024-11-15 12:13:01 +0545

    feat: add validation for transaction fee configuration in PaymentMode serializers

--- server/django/apps/voucher/serializers/payment_mode.py
+++ server/django/apps/voucher/serializers/payment_mode.py
@@ -1,10 +1,31 @@
 from rest_framework import serializers
 
 from apps.ledger.serializers import AccountListSerializer
-from apps.voucher.models import PaymentMode
+from apps.voucher.models import PaymentMode, TransactionFeeConfig
 
 
 class PaymentModeCreateSerializer(serializers.ModelSerializer):
+    def validate_transaction_fee_config(self, value):
+        if not value:
+            return
+
+        try:
+            TransactionFeeConfig(value)
+        except ValueError as e:
+            raise serializers.ValidationError(str(e))
+
+        return value
+
+    def validate(self, data):
+        if data.get("transaction_fee_config") and not data.get(
+            "transaction_fee_account"
+        ):
+            raise serializers.ValidationError(
+                "Transaction Fee Account is required if Transaction Fee Config is provided"
+            )
+
+        return
+
     class Meta:
         model = PaymentMode
         fields = [
@@ -22,6 +43,27 @@ class PaymentModeRetrieveSerializer(serializers.ModelSerializer):
     selected_transaction_fee_account_obj = AccountListSerializer(
         source="transaction_fee_account", read_only=True
     )
+
+    def validate_transaction_fee_config(self, value):
+        if not value:
+            return
+
+        try:
+            TransactionFeeConfig(value)
+        except ValueError as e:
+            raise serializers.ValidationError(str(e))
+
+        return value
+
+    def validate(self, data):
+        if data.get("transaction_fee_config") and not data.get(
+            "transaction_fee_account"
+        ):
+            raise serializers.ValidationError(
+                "Transaction Fee Account is required if Transaction Fee Config is provided"
+            )
+
+        return
 
     class Meta:
         model = PaymentMode
