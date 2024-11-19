68d68309309505c588ed64205b2051f490dd714c
Rohan Dhimal <drowhann@gmail.com>
2024-11-19 16:58:36 +0545

    feat: disable pagination, add transaction column, show dr and cr, update transactions link

--- web/app/src/pages/report/DayBook.vue
+++ web/app/src/pages/report/DayBook.vue
@@ -3,7 +3,7 @@ const route = useRoute()
 const router = useRouter()
 
 const date = ref(
-  route.query.date ? route.query.date : new Date().toISOString().substr(0, 10)
+  route.query.date ? route.query.date : new Date().toISOString().slice(0, 10)
 )
 
 const data = ref(null)
@@ -43,6 +43,13 @@ const columns = [
     label: 'Opening Balance',
     align: 'left',
     field: 'opening_balance',
+    sortable: true,
+  },
+  {
+    name: 'transaction',
+    label: 'Transaction',
+    align: 'left',
+    field: 'transaction',
     sortable: true,
   },
   {
@@ -100,6 +107,7 @@ function filterData() {
               flat
               bordered
               hide-pagination
+              :rows-per-page-options="[0]"
               :filter="searchQuery"
               :filter-method="filterData"
             >
@@ -128,13 +136,34 @@ function filterData() {
                   {{ props.row.account.name }}
                 </q-td>
               </template>
+              <template v-slot:body-cell-opening_balance="props">
+                <q-td>
+                  {{ props.row.opening_balance >= 0 ? 'Dr' : 'Cr' }}
+                  {{ Math.abs(props.row.opening_balance).toFixed(2) }}
+                </q-td>
+              </template>
+              <template v-slot:body-cell-closing_balance="props">
+                <q-td>
+                  {{ props.row.closing_balance >= 0 ? 'Dr' : 'Cr' }}
+                  {{ Math.abs(props.row.closing_balance).toFixed(2) }}
+                </q-td>
+              </template>
+              <template v-slot:body-cell-transaction="props">
+                <q-td>
+                  {{
+                    (
+                      props.row.opening_balance - props.row.closing_balance
+                    ).toFixed(2)
+                  }}
+                </q-td>
+              </template>
               <template v-slot:body-cell-action="props">
                 <q-td>
                   <RouterLink
                     style="text-decoration: none"
                     class="text-blue-6"
                     target="_blank"
-                    :to="`/report/transactions?account=${props.row.account.id}&start_date=${date}&end_date=${date}`"
+                    :to="`/account/${props.row.account.id}/view/?start_date=${date}&end_date=${date}`"
                   >
                     View Transactions
                   </RouterLink>
