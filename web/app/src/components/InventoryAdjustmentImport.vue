364e6353ef41bee5e9d64d490b4c7e008403412e
Mijashh <amijash@gmail.com>
2024-11-26 16:39:55 +0545

    refactor: update sample excel content in InventoryAdjustmentImport

--- web/app/src/components/InventoryAdjustmentImport.vue
+++ web/app/src/components/InventoryAdjustmentImport.vue
@@ -68,10 +68,10 @@ const onSampleDownload = async () => {
     { v: 'Description', s: { font: { bold: true } } },
   ];
   const row2 = [
-    { v: '9789937780414', },
-    { v: 10, },
-    { v: 100, },
-    { v: 'Test123', },
+    { v: 'Code', },
+    { v: 0, },
+    { v: 0, },
+    { v: 'Description', },
     { v: 'Note: Remove the row and fill in your data. Please Do not remove any columns. if you don\'t want to insert data in any particular cell then leave it empty. Category column is optional. If the values of Can be Sold and Can be Purchased columns are t or T then it will be set to true, leaving the values to be something else or empty will result in those fields being false.' }
   ]
   const ws = XLSX.utils.aoa_to_sheet([row, row2])
