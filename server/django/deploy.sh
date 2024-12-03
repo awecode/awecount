d5582513499dfc9aff229c2da9a96e5d17c092f7
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2024-12-03 22:54:16 +0545

    chore: temporarily comment out migration command in deploy and entrypoint scripts

--- server/django/deploy.sh
+++ server/django/deploy.sh
@@ -1,5 +1,5 @@
 . /home/awex/env/bin/activate
 cd /home/awex/app/
 pip install -r requirements/prod.txt | grep -v 'Requirement already satisfied' | grep -v 'Cleaning up...'
-python manage.py migrate -v 0
+# python manage.py migrate -v 0
 sudo /usr/bin/supervisorctl restart awecount:*
