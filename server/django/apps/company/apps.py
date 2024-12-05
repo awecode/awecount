62634d5798c4bcb05556481a5a9429cd8a20a46a
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2024-12-05 18:56:19 +0545

    refactor: add comments to clarify purpose of global permissions caching in CompanyConfig

--- server/django/apps/company/apps.py
+++ server/django/apps/company/apps.py
@@ -9,6 +9,9 @@ class CompanyConfig(AppConfig):
 
         from .models import get_default_permissions
 
+        # The ultimate purpose of this is to scan and check syntax of all permission metas.
+        # Later on maybe, we can utilize the cached data.
+
         global_permissions = get_default_permissions()
         cache.set("global_permissions", global_permissions)
 
