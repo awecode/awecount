9aafc8e58f730c2b90c46ee2e3553d0e7a0f8625
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2024-12-09 12:41:54 +0545

    refactor: add example environment settings and clean up configuration files

--- server/django/awecount/settings/dev.py
+++ server/django/awecount/settings/dev.py
@@ -1,83 +1,27 @@
-from .base import INSTALLED_APPS, MIDDLEWARE, REST_FRAMEWORK
+######################################################################
+# General
+######################################################################
+SECRET_KEY = "_(bu729mlq!a@9#b9#-s@s%$^-d7qq%h*d2wsfzp6nes_a!$9&"
 
 DEBUG = True
 
-SECRET_KEY = "4oiwq=^7+@ub=shgbo#=gj17jveh+_*9tuqfx5fa-1orav(7xx"
 
-INSTALLED_APPS += ["debug_toolbar", "django_extensions"]
+######################################################################
+# Domains
+######################################################################
+ALLOWED_HOSTS = ["*"]
 
-MIDDLEWARE += [
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
-]
+CORS_ALLOWED_ALL_ORIGINS = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
-REST_FRAMEWORK["DEFAULT_AUTHENTICATION_CLASSES"] += [
-    "rest_framework.authentication.SessionAuthentication"
-]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000", "http://localhost:8000"]
 
-INTERNAL_IPS = ["127.0.0.1"]
+######################################################################
+# Email
+######################################################################
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "awecount",
-        "USER": "postgres",
-        "PASSWORD": "password",
-        "HOST": "",
-        "PORT": "",
-    }
-}
-CORS_ORIGIN_WHITELIST = (
-    "http://localhost:8080",
-    "http://localhost:8081",
-    "http://127.0.0.1:8080",
-    "http://127.0.0.1:9000",
-    "http://localhost:9000",
-)
 
-CSRF_TRUSTED_ORIGINS = (
-    "http://localhost:8080",
-    "http://localhost:8081",
-    "http://127.0.0.1:8080",
-    "http://127.0.0.1:9000",
-    "http://localhost:9000",
-)
-
-CORS_ALLOWED_ORIGINS = (
-    "http://localhost:8080",
-    "http://localhost:8081",
-    "http://127.0.0.1:8080",
-    "http://127.0.0.1:9000",
-    "http://localhost:9000",
-)
-
-BASE_URL = "http://localhost:8080/"
-
-# LOGGING = {
-#     "version": 1,
-#     "disable_existing_loggers": False,
-#     'formatters': {
-#         'standard': {
-#             'format': "[%(asctime)s] %(levelname)s %(message)s",
-#             'datefmt': "%d/%b/%Y %H:%M:%S"
-#         }
-#     },
-#     "handlers": {
-#         "file": {
-#             "level": "DEBUG",
-#             "class": "logging.FileHandler",
-#             "filename": "./debug.log",
-#             'formatter': 'standard'
-#         },
-#     },
-#     "loggers": {
-#         "django": {
-#             "handlers": ["file"],
-#             "level": "INFO",
-#             "propagate": True,
-#             'formatter': 'standard'
-#         },
-#     },
-# }
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+######################################################################
+# Allauth
+######################################################################
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = "http"
