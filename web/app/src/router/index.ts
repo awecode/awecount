b44c8eac8a845f34338c6b85b1f4e5ee977c81c5
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2024-12-10 17:55:48 +0545

    feat: implement role-based access control (RBAC) in router with authentication checks

--- web/app/src/router/index.ts
+++ web/app/src/router/index.ts
@@ -1,5 +1,5 @@
 import { defineRouter } from '#q-app/wrappers'
-import { useLoginStore } from 'stores/login-info'
+
 import { createMemoryHistory, createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 
 import routes from './routes'
@@ -13,31 +13,6 @@ export default defineRouter((/* { store, ssrContext } */) => {
     routes,
     scrollBehavior: () => ({ left: 0, top: 0 }),
     history: createHistory(import.meta.env.VUE_ROUTER_BASE),
-  })
-
-  // Token validation
-  Router.beforeEach((to, from, next) => {
-    const store = useLoginStore()
-    if (to.path === '/') {
-      if (store.token) {
-        next('/dashboard')
-      } else {
-        next()
-      }
-    } else if (to.path !== '/login' && store.token === null) {
-      next('/login')
-    } else {
-      // Should check if back btn is pressed from Nopermission page
-      // TODO: Not handled when jumping from one no permission page to another
-      if (
-        to.fullPath === window.history.state.current
-        && from.name === 'NoPermission'
-      ) {
-        next(window.history.state.back)
-      } else {
-        next()
-      }
-    }
   })
 
   return Router
