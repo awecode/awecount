128a221e17faf8723546c4627d5acd66a6d029ae
SBOANDIAKL <sonikbadal@gmail.com>
2024-12-10 11:37:06 +0545

    feat: add more validations on document uplaod/parsing logic

--- web/app/src/pages/bank/reconciliation/IndexPage.vue
+++ web/app/src/pages/bank/reconciliation/IndexPage.vue
@@ -20,16 +20,20 @@
         <q-form @submit="submitStatement">
 
           <n-auto-complete v-model="selectedBankAccount" :options="bankAccounts" endpoint="v1/bank-reconciliation/create-defaults" label="Bank Accounts" />
-          <q-file bottom-slots v-model="statementSheet" label="Statement Document" counter max-files="1" accept=".csv, .xlsx, .xls" class="q-mb-md">
+          <q-file bottom-slots v-model="statementSheet" label="Statement Document" counter max-files="1" accept=".csv, .xlsx, .xls" class="q-mb-md" @update:model-value="parseExcelFile">
             <template v-slot:prepend>
               <q-icon name="cloud_upload" @click.stop.prevent />
             </template>
             <template v-slot:append>
-              <q-icon v-if="statementSheet" name="close" @click.stop.prevent="statementSheet = null" class="cursor-pointer" />
+              <q-icon v-if="statementSheet" name="close" @click.stop.prevent="removeStatement" class="cursor-pointer" />
             </template>
 
             <template v-slot:hint>
-              <div class="text-gray-600">Upload your statement document here</div>
+              <div v-if="!statementSheet" class="text-gray-600">Upload your statement document here</div>
+              <div v-else-if="isStatementProcessing" class="text-gray-600">Processing...</div>
+              <div v-else-if="hasError" class="text-red-600 text-xs">Error parsing the file</div>
+              <div v-else-if="toHaveHeaders.find(header => !statementHeaders.has(header))" class="text-red-600 text-xs">Couldn't find header columns: {{
+                toHaveHeaders.filter(header => !statementHeaders.has(header)).join(', ') }}</div>
             </template>
           </q-file>
 
@@ -41,7 +45,8 @@
           </div>
 
           <div class="text-right !mt-6">
-            <q-btn type="submit" label="Upload" color="green"></q-btn>
+            <q-btn type="submit" label="Upload" color="green"
+              :disable="!selectedBankAccount || !statementSheet || isStatementProcessing || toHaveHeaders.find(header => !statementHeaders.has(header))" />
           </div>
         </q-form>
       </q-card>
@@ -51,13 +56,19 @@
 
 <script setup lang="ts">
 import * as XLSX from 'xlsx'
+import { Ref } from 'vue'
 
 const selectedBankAccount = ref(null)
 const bankAccounts = ref([])
-const startDate = ref(null)
-const endDate = ref(null)
+const startDate = ref()
+const endDate = ref()
 const statementPrompt = ref(true)
 const statementSheet = ref(null)
+const statementHeaders = ref(new Set())
+
+const statementData: Ref<Record<string, any>[]> = ref([])
+const isStatementProcessing = ref(false)
+const hasError = ref(false)
 
 const endpoint = 'v1/bank-reconciliation/banks/'
 
@@ -67,13 +78,18 @@ useApi(endpoint).then((response) => {
 
 type MappingFunction = (header: string) => string
 
+const toHaveHeaders = ['date', 'dr_amount', 'cr_amount', 'balance']
+
 const headerMappings: { [key: string]: string | MappingFunction } = {
   id: 'id',
   'txn date': 'date',
   'chequeno': 'cheque_no',
+  'chequeno.': 'cheque_no',
   'debit(npr)': 'dr_amount',
   'credit(npr)': 'cr_amount',
   'balance(npr)': 'balance',
+  'txn no.': 'txn_no',
+  'txn no': 'txn_no',
 }
 
 function mapHeaders(headers: string[]): string[] {
@@ -90,13 +106,15 @@ function mapHeaders(headers: string[]): string[] {
   })
 }
 
+const removeStatement = () => {
+  statementSheet.value = null
+  statementData.value = []
+  statementHeaders.value = new Set()
+}
+
 
 const submitStatement = async () => {
-  if (!statementSheet.value) {
-    return
-  }
-  const data = await parseExcelFile(statementSheet.value)
-  if (!selectedBankAccount.value || !statementSheet.value) {
+  if (!selectedBankAccount.value || !statementData.value.length) {
     return
   }
 
@@ -104,7 +122,7 @@ const submitStatement = async () => {
     method: 'POST',
     body: {
       bank_account: selectedBankAccount.value,
-      transactions: data,
+      transactions: statementData.value,
       start_date: startDate.value,
       end_date: endDate.value,
     }
@@ -113,11 +131,47 @@ const submitStatement = async () => {
   })
 }
 
+function parseDate(dateString: string | null): string {
+  if (!dateString) {
+    throw new Error('Date string is empty')
+  }
+  const delimiters = ['/', '-', '.']
+  let parts, day, month, year
+
+  // Detect the delimiter
+  const delimiter = delimiters.find((d) => dateString.includes(d))
+  if (!delimiter) {
+    throw new Error(`Unsupported date format: ${dateString}`)
+  }
+
+  parts = dateString.split(delimiter).map(Number)
+
+  if (parts.length === 3) {
+    if (parts[0] > 31) {
+      [year, month, day] = parts
+    } else if (parts[2] > 31) {
+      [month, day, year] = parts
+    } else {
+      [day, month, year] = parts
+    }
+
+    if (year < 100) {
+      year += year > 50 ? 1900 : 2000
+    }
+
+    return new Date(year, month - 1, day).toISOString().split('T')[0]
+  }
+  throw new Error(`Unsupported date format: ${dateString}`)
+}
+
 
 
 function parseXLSXSheet(sheetData: any[][]): any[] {
   let headerRowIndex = sheetData.findIndex(row =>
-    row.some(cell => String(cell).toLowerCase().includes('debit'))
+    row.some(cell => {
+      const lowerCasedCell = String(cell).toLowerCase()
+      return lowerCasedCell.includes('debit') || lowerCasedCell.includes('credit')
+    })
   )
   // If no header row found, throw an error
   if (headerRowIndex === -1) {
@@ -134,21 +188,25 @@ function parseXLSXSheet(sheetData: any[][]): any[] {
 
   headerRow = mapHeaders(headerRow)
 
+  statementHeaders.value = new Set([...statementHeaders.value, ...headerRow])
+
   interface ParsedData {
     [id: string]: any
   }
 
   const data: ParsedData[] = []
+  debugger
 
   sheetData.slice(headerRowIndex + 1).forEach(row => {
     const rowData: ParsedData = {}
     row.forEach((cell, index) => {
       rowData[headerRow[index]] = cell
+
       if (headerRow[index] === 'date') {
         if (/^\d{4}-\d{2}-\d{2}$/.test(cell)) {
           rowData['date'] = cell
         } else {
-          rowData['date'] = new Date(cell).toISOString().split('T')[0]
+          rowData['date'] = parseDate(cell)
         }
       }
 
@@ -179,19 +237,28 @@ function fileToArrayBuffer(file: File): Promise<ArrayBuffer> {
 }
 
 
-async function parseExcelFile(file: File): Promise<Record<string, any>[]> {
-  const data: Record<string, any>[] = []
-  await fileToArrayBuffer(file).then((arrayBuffer) => {
-    const workbook = XLSX.read(arrayBuffer, { type: 'array' })
-    workbook.SheetNames.forEach((sheetName) => {
-      const worksheet = workbook.Sheets[sheetName]
-      const sheetData = XLSX.utils.sheet_to_json(worksheet, { header: 1 }) as any[][]
-      const parsedData = parseXLSXSheet(sheetData)
-      const filteredData = parsedData.filter((row) => Object.keys(row).length > 0)
-      data.push(...filteredData)
+async function parseExcelFile(file: File): Promise<void> {
+  isStatementProcessing.value = true
+  statementData.value = []
+  hasError.value = false
+  try {
+
+    await fileToArrayBuffer(file).then((arrayBuffer) => {
+      const workbook = XLSX.read(arrayBuffer, { type: 'array' })
+      workbook.SheetNames.forEach((sheetName) => {
+        const worksheet = workbook.Sheets[sheetName]
+        const sheetData = XLSX.utils.sheet_to_json(worksheet, { header: 1 }) as any[][]
+        const parsedData = parseXLSXSheet(sheetData)
+        const filteredData = parsedData.filter((row) => Object.keys(row).length > 0)
+        statementData.value.push(...filteredData)
+      })
     })
-  })
-  return data
+  }
+  catch (e) {
+    console.log(e)
+    hasError.value = true
+  }
+  isStatementProcessing.value = false
 }
 
 </script>
