bbce1f97e9001e5c305c27f6a0ce9832f32991ca
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2024-12-13 10:39:01 +0545

    fix: update default database name in settings to 'awecount-db'

--- server/django/awecount/settings/base.py
+++ server/django/awecount/settings/base.py
@@ -141,7 +141,7 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.environ.get("POSTGRES_DATABASE", "sponsorvista"),
+            "NAME": os.environ.get("POSTGRES_DATABASE", "awecount-db"),
             "USER": os.environ.get("POSTGRES_USER", "postgres"),
             "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "postgres"),
             "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
