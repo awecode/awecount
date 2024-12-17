f1acd1fae30288079e4ded82651810e302d7df43
Rohan Dhimal <drowhann@gmail.com>
2024-12-17 16:24:01 +0545

    refactor: remove SalesVoucherPdfView URL pattern from voucher routes

--- server/django/apps/voucher/urls.py
+++ server/django/apps/voucher/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("pdf/sale-voucher/<int:pk>", views.SalesVoucherPdfView.as_view()),
     path("upload-file/", views.FileUploadView.as_view(), name="upload-file"),
     path("v1/invoice-setting-update/", views.InvoiceSettingUpdateView.as_view()),
 ]
