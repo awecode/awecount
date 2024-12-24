2f1f5e710d8c1c2b82c3f0d2d30b743c89ddacdc
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2024-12-24 10:27:21 +0545

    feat: extend RouteMeta to include breadcrumb structure for enhanced navigation

--- web/app/src/router/vue-router.d.ts
+++ web/app/src/router/vue-router.d.ts
@@ -5,6 +5,10 @@ export {} // This is To ensure it is treated as a module and prevent type reset
 declare module 'vue-router' {
   interface RouteMeta {
     breadcrumbs?: Array<string>
+    breadcrumb: {
+      label: string
+      to?: string
+    }
     auth?: {
       protected?: boolean
       roles?: Array<'owner' | 'admin' | 'member'>
