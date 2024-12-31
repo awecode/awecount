099844730018400b040246c0e3731b56e6674709
Dipesh Acharya <dipesh@awecode.com>
2024-12-31 17:14:20 +0545

    chore: cleanup

--- server/django/appmigrate.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-git checkout 8e66922
-python manage.py migrate
-git checkout bb1ecf8
-python manage.py migrate
-git checkout f083e04
-python manage.py migrate
-git switch main
--- server/django/deploy.sh
+++
@@ -1,5 +0,0 @@
-. /home/awex/env/bin/activate
-cd /home/awex/app/
-pip install -r requirements/prod.txt | grep -v 'Requirement already satisfied' | grep -v 'Cleaning up...'
-python manage.py migrate -v 0
-sudo /usr/bin/supervisorctl restart awecounting:*
