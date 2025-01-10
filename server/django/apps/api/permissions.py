d7eddfa759558226cf09a6fa87bb7976190b48f9
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2025-01-10 15:48:41 +0545

    feat: enhance APIKeyPermission to map actions and methods for improved permission handling

--- server/django/apps/api/permissions.py
+++ server/django/apps/api/permissions.py
@@ -14,7 +14,31 @@ class APIKeyPermission(BasePermission):
         if request.method in SAFE_METHODS and not request.method == "GET":
             return True
 
+        DEFAULT_ACTION_MAPPING = {
+            "create": "create",
+            "list": "view",
+            "retrieve": "view",
+            "update": "modify",
+            "partial_update": "modify",
+            "destroy": "delete",
+        }
+
+        DEFAULT_METHOD_MAPPING = {
+            "POST": "create",
+            "GET": "view",
+            "PUT": "modify",
+            "PATCH": "modify",
+            "DELETE": "delete",
+        }
+
         model = view.model.__name__.lower()
-        action = view.action
+        action = None
+
+        # Get the action from the view.action and if it is not present in the
+        # DEFAULT_ACTION_MAPPING, then use the view.action itself.
+        if hasattr(view, "action"):
+            action = DEFAULT_ACTION_MAPPING.get(view.action, view.action)
+        else:
+            action = DEFAULT_METHOD_MAPPING.get(request.method)
 
         return api_key.permissions.get(model, {}).get(action, False)
