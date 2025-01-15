6f595e5b80a56b29e7f3e2ad95255552f3072531
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2025-01-15 11:37:11 +0545

    feat: add admin interface for managing API keys

---
+++ server/django/apps/api/admin.py
@@ -0,0 +1,46 @@
+from django.contrib import admin, messages
+from django.utils.translation import gettext_lazy as _
+
+from apps.api.models import APIKey
+
+
+@admin.register(APIKey)
+class APIKeyAdmin(admin.ModelAdmin):
+    list_display = (
+        "prefix",
+        "name",
+        "created_at",
+        "expiry_date",
+        "has_expired",
+        "revoked",
+    )
+    list_filter = ("created_at", "user", "revoked")
+    search_fields = ("user__email", "name", "prefix")
+
+    def get_readonly_fields(self, request, obj=None):
+        fields = ("prefix",)
+        if obj is not None and obj.revoked:
+            fields = fields + ("name", "revoked", "expiry_date")
+
+        return fields
+
+    def save_model(
+        self,
+        request,
+        obj: APIKey,
+        form=None,
+        change=False,
+    ):
+        created = not obj.pk
+
+        if created:
+            key = self.model.objects.assign_key(obj)
+            obj.save()
+            message = _(
+                "The API key for {} is: {}. ".format(obj.name, key)
+                + "Please store it somewhere safe: "
+                + "you will not be able to see it again."
+            )
+            messages.add_message(request, messages.WARNING, message)
+        else:
+            obj.save()
