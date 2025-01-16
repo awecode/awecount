46123b56e705914bfb7e1b5181e5c2014a4cdeb2
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2025-01-16 15:04:55 +0545

    fix: remove duplicate emit and store declartion

--- web/app/src/components/date/DateRangePicker.vue
+++ web/app/src/components/date/DateRangePicker.vue
@@ -18,15 +18,6 @@ const props = defineProps({
 
 const emit = defineEmits(['update:startDate', 'update:endDate', 'filter'])
 
-// const showClear = computed(() => {
-// return !this.hideClear && (this.value0 || this.value1)
-// return false
-// })
-
-const emit = defineEmits(['update:startDate', 'update:endDate', 'filter'])
-
-const store = useLoginStore()
-
 const store = useLoginStore()
 
 const menuDom = ref(null)
