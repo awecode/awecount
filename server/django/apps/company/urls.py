cdfad87d3e40797c45ceb5a82a72e0bbdfffa3f9
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2025-02-20 18:26:54 +0545

    refactor: reorganize company URLs

--- server/django/apps/company/urls.py
+++ server/django/apps/company/urls.py
@@ -1,62 +1,17 @@
 from django.urls import re_path
 
 from apps.company.views import (
-    CompanyViewset,
     CompanyInvitationsViewset,
     CompanyJoinEndpoint,
     CompanyPermissionEndpoint,
+    CompanyViewset,
     UserCompaniesEndpoint,
     UserCompanyInvitationsViewSet,
     UserCompanySwitchEndpoint,
 )
 
 urlpatterns = [
-    re_path(
-        r"^api/company/(?P<company_slug>[-\w]+)/invitations/$",
-        CompanyInvitationsViewset.as_view(
-            {
-                "get": "list",
-                "post": "create",
-            }
-        ),
-        name="company-invitations",
-    ),
-    re_path(
-        r"^api/company/(?P<company_slug>[-\w]+)/invitations/(?P<pk>[0-9a-f-]+)/$",
-        CompanyInvitationsViewset.as_view(
-            {
-                "delete": "destroy",
-            }
-        ),
-        name="company-invitation-detail",
-    ),
-    re_path(
-        r"^api/company/(?P<company_slug>[-\w]+)/join/(?P<pk>[0-9a-f-]+)/$",
-        CompanyJoinEndpoint.as_view(),
-        name="company-join",
-    ),
-
-    re_path(
-        r"^api/me/invitations/$",
-        UserCompanyInvitationsViewSet.as_view(
-            {
-                "get": "list",
-                "post": "create",
-            }
-        ),
-        name="user-company-invitations",
-    ),
-    re_path(
-        r"^api/me/companies/$",
-        UserCompaniesEndpoint.as_view(),
-        name="user-companies",
-    ),
-    re_path(
-        r"^api/me/switch-company/$",
-        UserCompanySwitchEndpoint.as_view(),
-        name="switch-company",
-    ),
-
+    # Company-related URLs
     re_path(
         r"^api/company/$",
         CompanyViewset.as_view(
@@ -89,5 +44,51 @@ urlpatterns = [
         r"^api/company/(?P<company_slug>[-\w]+)/permissions/$",
         CompanyPermissionEndpoint.as_view(),
         name="company-permissions",
+    ),
+    re_path(
+        r"^api/company/(?P<company_slug>[-\w]+)/invitations/$",
+        CompanyInvitationsViewset.as_view(
+            {
+                "get": "list",
+                "post": "create",
+            }
+        ),
+        name="company-invitations",
+    ),
+    re_path(
+        r"^api/company/(?P<company_slug>[-\w]+)/invitations/(?P<pk>[0-9a-f-]+)/$",
+        CompanyInvitationsViewset.as_view(
+            {
+                "delete": "destroy",
+            }
+        ),
+        name="company-invitation-detail",
+    ),
+    re_path(
+        r"^api/company/(?P<company_slug>[-\w]+)/join/(?P<pk>[0-9a-f-]+)/$",
+        CompanyJoinEndpoint.as_view(),
+        name="company-join",
+    ),
+    # User-related URLs
+    # TODO: api/me -> api/user/me
+    re_path(
+        r"^api/me/invitations/$",
+        UserCompanyInvitationsViewSet.as_view(
+            {
+                "get": "list",
+                "post": "respond",
+            }
+        ),
+        name="user-company-invitations",
+    ),
+    re_path(
+        r"^api/me/companies/$",
+        UserCompaniesEndpoint.as_view(),
+        name="user-companies",
+    ),
+    re_path(
+        r"^api/me/switch-company/$",
+        UserCompanySwitchEndpoint.as_view(),
+        name="switch-company",
     ),
 ]
