dfa162161b4a701ea57ad1bd3d1ea41aeb0510fe
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2025-02-21 14:32:28 +0545

    fix: use SimpleRouter instead of DeafultRouter

--- server/django/apps/api/urls.py
+++ server/django/apps/api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import include, re_path
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 
 from apps.api.views import APIKeyViewSet
 
 
-router = DefaultRouter()
-router.register(r'api-tokens', APIKeyViewSet, basename='api-tokens')
+router = SimpleRouter()
+router.register(r"api-tokens", APIKeyViewSet, basename="api-tokens")
 
 urlpatterns = [
-    re_path(r'^api/company/(?P<company_slug>[-\w]+)/', include(router.urls)),
+    re_path(r"^api/company/(?P<company_slug>[-\w]+)/", include(router.urls)),
 ]
