6827775bf98e268bebbaab570460ce9625b62bb0
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2025-02-24 15:38:13 +0545

    feat: add user profile management endpoints for retrieving, updating, and deactivating user

--- server/django/apps/users/urls.py
+++ server/django/apps/users/urls.py
@@ -1,11 +1,24 @@
 from django.urls import re_path
 
-from .views import UpdateUserOnBoardedEndpoint
+from .views import UpdateUserOnBoardedEndpoint, UserViewset
 
 urlpatterns = [
     re_path(
         r"^api/user/me/onboarded/$",
         UpdateUserOnBoardedEndpoint.as_view(),
         name="update-user-onboarded",
+    ),
+    re_path(
+        r"^api/me/$",
+        UserViewset.as_view({
+            "get": "retrieve",
+            "patch": "update",
+        }),
+        name="user",
+    ),
+    re_path(
+        r"^api/me/deactivate/$",
+        UserViewset.as_view({"patch": "deactivate"}),
+        name="deactivate-user",
     ),
 ]
--- server/django/apps/users/views.py
+++ server/django/apps/users/views.py
@@ -1,7 +1,36 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import status, views
 from rest_framework.response import Response
 
 from apps.users.models import Profile
+
+from rest_framework import mixins, viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from apps.users.serializers import UserSerializer
+
+
+class UserViewset(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet,
+):
+    model = get_user_model()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.model.objects.filter(id=self.request.user.id)
+
+    def get_object(self):
+        return self.request.user
+
+    @action(detail=False, methods=["patch"])
+    def deactivate(self, request):
+        self.get_object().is_active = False
+        self.get_object().save()
+        return Response({"message": "User deactivated successfully"}, status=status.HTTP_200_OK)
 
 
 class UpdateUserOnBoardedEndpoint(views.APIView):
