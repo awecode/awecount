3003b6ce13d18e84dd6631cac309e35aed2418e7
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2025-04-18 13:35:13 +0545

    feat: add index page

---
+++ web/app/app/pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import LandingPage from '@/components/views/LandingPage.vue'
+
+definePageMeta({
+  layout: false,
+})
+</script>
+
+<template>
+  <LandingPage />
+</template>
