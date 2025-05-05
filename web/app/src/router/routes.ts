d36b6105badb732b2081709e5917cd0b795fb107
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2025-05-05 11:32:36 +0545

    feat: add redirect for company index route

--- web/app/src/router/routes.ts
+++ web/app/src/router/routes.ts
@@ -1,6 +1,13 @@
-import type { RouteRecordRaw } from 'vue-router'
+import type { RouteLocationNormalized, RouteRecordRaw } from 'vue-router'
 
 const companyRoutes: RouteRecordRaw[] = [
+  {
+    path: '',
+    name: 'company-index',
+    redirect: (to: RouteLocationNormalized) => {
+      return { name: 'company-dashboard', params: { company: to.params.company } }
+    },
+  },
   {
     path: 'dashboard',
     name: 'company-dashboard',
