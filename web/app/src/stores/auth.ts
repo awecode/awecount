a4cec8fadfc888d28beb1ef279d5cc7727eceff5
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2025-05-22 16:48:17 +0545

    fix: resolve issue with password reset

--- web/app/src/stores/auth.ts
+++ web/app/src/stores/auth.ts
@@ -235,7 +235,14 @@ export const useAuthStore = defineStore(
     }
 
     const resetPassword = async (data: Record<string, any>) => {
-      return await _request(URLs.RESET_PASSWORD, { method: 'POST', body: data })
+      try {
+        return await _request(URLs.RESET_PASSWORD, { method: 'POST', body: data })
+      } catch (error) {
+        if (error.response?.status === 401) {
+          return error.response._data
+        }
+        throw error
+      }
     }
 
     const checkEmail = async (email: string) => {
